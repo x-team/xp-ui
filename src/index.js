46b29de8c6cd140ed0b64b84d76f4f82e6f4b684
Josh Johnston <josh@x-team.com>
2017-09-21 14:22:42 +1000

    added webpack config to build a standalone bundle

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import ErrorBox from './components/ErrorBox'
+import PageTitle from './components/PageTitle'
+import XIcon from './components/XIcon'
+
+export { 
+  ErrorBox,
+  PageTitle,
+  XIcon
+}
+
---
+++ webpack.standalone.js
@@ -0,0 +1,24 @@
+const path = require('path')
+const base = require('./webpack.config')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve('./lib'),
+    filename: 'auto-ui.js',
+    library: 'auto-ui',
+    libraryTarget: 'commonjs2',
+    umdNamedDefine: true
+  },
+  externals: {
+    react: {
+      root: 'React',
+      commonjs2: 'react'
+    },
+    'react-dom': {
+      root: 'ReactDOM',
+      commonjs2: 'react-dom'
+    },
+  },
+  module: base.module
+}
