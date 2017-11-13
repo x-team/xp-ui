932ae39776654da9c596519277dd536a2d58832e
Agustin Falco <agustin.falco@x-team.com>
2017-11-13 15:24:13 -0300

    Removed unnecesary div and added snapshot.

--- src/components/ApplicantListFilter.js
+++ src/components/ApplicantListFilter.js
@@ -28,28 +28,26 @@ class ApplicantListFilter extends PureComponent<Props> {
   render () {
     const { handleCheck, pending, accepted, excluded } = this.props
     return (
-      <div>
-        <form className={optionContainer}>
-          <div className={option}>
-            <label>
-              <input type='checkbox' name='pending' onChange={handleCheck} />
-              <span>Pending ({pending})</span>
-            </label>
-          </div>
-          <div className={option}>
-            <label>
-              <input type='checkbox' name='accepted' onChange={handleCheck} />
-              <span>Accepted ({accepted})</span>
-            </label>
-          </div>
-          <div className={option}>
-            <label>
-              <input type='checkbox' name='excluded' onChange={handleCheck} />
-              <span>Excluded ({excluded})</span>
-            </label>
-          </div>
-        </form>
-      </div>
+      <form className={optionContainer}>
+        <div className={option}>
+          <label>
+            <input type='checkbox' name='pending' onChange={handleCheck} />
+            <span>Pending ({pending})</span>
+          </label>
+        </div>
+        <div className={option}>
+          <label>
+            <input type='checkbox' name='accepted' onChange={handleCheck} />
+            <span>Accepted ({accepted})</span>
+          </label>
+        </div>
+        <div className={option}>
+          <label>
+            <input type='checkbox' name='excluded' onChange={handleCheck} />
+            <span>Excluded ({excluded})</span>
+          </label>
+        </div>
+      </form>
     )
   }
 }
