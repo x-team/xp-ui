c55e90a922e2914ee151375b6d7042cced1e7f02
Richa Bhardwaj <bhardwaj.richa17@gmail.com>
2017-11-13 15:43:17 +0530

    included text component in index

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import ErrorBox from './components/ErrorBox'
 import PageTitle from './components/PageTitle'
 import XIcon from './components/XIcon'
 import CollapsibleSection from './components/CollapsibleSection'
+import Text from './components/Text'
 
 export {
   ErrorBox,
   PageTitle,
   XIcon,
-  CollapsibleSection
+  CollapsibleSection,
+  Text
 }
