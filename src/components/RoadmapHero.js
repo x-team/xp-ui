4ece21cba3756db2d670173bff4a5bf61e4f68f2
Josh Johnston <josh@x-team.com>
2017-11-20 12:32:16 +1100

    cleanup

--- src/components/RoadmapHero.js
+++ src/components/RoadmapHero.js
@@ -17,20 +17,20 @@ type Props = {
 
 const Root = elem.div(cmz(`
   & {
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: space-around;
-    align-items: center;
+    display: flex
+    flex-wrap: wrap
+    justify-content: space-around
+    align-items: center
   }
 
   @media screen and (max-width: ${breakpoints.sm}) {
     & {
-      margin: 0 0 35px 0;
+      margin: 0 0 35px 0
     }
   }
 `))
 
-const HeroHeading = elem.div(cmz(`width: 60%;`))
+const HeroHeading = elem.div(cmz('width: 60%'))
 const HeroImage = elem.img()
 
 class RoadmapHero extends PureComponent<Props> {
