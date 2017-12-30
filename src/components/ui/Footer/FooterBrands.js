a7e6fa5321aee93a071c1a0fa6a225a8e6db79f9
Nick Plekhanov <nickatgit@yandex.com>
2017-12-30 05:02:26 +0300

    Refactor ‘FooterBrands’ to match design
    Also update spec of usage.

--- src/components/ui/Footer/FooterBrands.js
+++ src/components/ui/Footer/FooterBrands.js
@@ -2,8 +2,8 @@
 
 import React, { PureComponent } from 'react'
 
-import { mediaQueries } from '../../../styles/theme'
-import typo from '../../../styles/typo'
+import theme, { mediaQueries } from '../../../styles/theme'
+import { typeface } from '../../../styles/typo'
 
 const cmz = require('cmz')
 
@@ -18,78 +18,105 @@ type Props = {
 
 const cx = {
   logos: cmz(`
-    list-style: none;
-    margin: 0;
-    padding: 0;
-    display: flex;
-  `,
-  `
-  ${mediaQueries.medium} {
     & {
-      display: block
+      list-style: none
+      margin: 0
+      padding: 0
+      display: flex
     }
-  }
+
+    ${mediaQueries.medium} {
+      & {
+        display: block
+      }
+    }
   `),
+
   brand: cmz(`
-    line-height: 3rem;
-    margin-right: 1rem;
-    display: inline-block;
-    vertical-align: middle;
-  `,
-  `
-  &:last-child {
-    margin-right: 0;
-  }
-  ${mediaQueries.desktop} {
     & {
-      line-height: 4rem;
-      margin-right: 3rem;
+      line-height: 3rem
+      margin-right: 1rem
+      display: inline-block
+      vertical-align: middle
     }
-  }
+
+    &:last-child {
+      margin-right: 0
+    }
+
+    ${mediaQueries.desktop} {
+      & {
+        line-height: 4rem
+        margin-right: 3rem
+      }
+    }
   `),
+
   image: cmz(`
-    max-height: 4rem;
-    display: inline-block;
-    vertical-align: middle;
-    max-width: 100%;
-    opacity: 0.8;
-    transition: all .3s ease-in;
+    max-height: 4rem
+    display: inline-block
+    vertical-align: middle
+    max-width: 100%
+    opacity: 1
+    transition: all .3s ease-in
   `),
+
   brandContainer: cmz(`
-    margin: 1.25rem auto;
+    ${mediaQueries.medium} {
+      & {
+        margin: 1.5rem auto
+      }
+    }
+
+    ${mediaQueries.desktop} {
+      & {
+        margin: 1.25rem auto
+      }
+    }
   `),
+
   message: cmz(
+    typeface.semiHeading,
     `
-    font-size: .875rem;
-    font-weight: 400;
-    text-transform: uppercase;
-    opacity: .5;
-    margin: 0;
-  `,
-    typo.baseText
+      & {
+        font-size: 14px
+        font-weight: 400
+        color: ${theme.baseDarker}
+        text-transform: uppercase
+        opacity: .5
+        margin: 0
+      }
+
+      ${mediaQueries.medium} {
+        & {
+          line-height: 1.715
+        }
+      }
+    `
   )
 }
 
-export default class FooterBrands extends PureComponent<Props> {
+class FooterBrands extends PureComponent<Props> {
   render () {
     const { message, brands } = this.props
+
     return (
       <div>
         <h4 className={cx.message}>{message}</h4>
         <div className={cx.brandContainer}>
           <ul className={cx.logos}>
-            {brands.map((item, id) => {
-              return (
-                <li className={cx.brand} key={id}>
-                  <a href={item.url}>
-                    <img className={cx.image} src={item.image} alt={item.title} />
-                  </a>
-                </li>
-              )
-            })}
+            {brands.map((item, id) => (
+              <li className={cx.brand} key={id}>
+                <a href={item.url}>
+                  <img className={cx.image} src={item.image} alt={item.title} />
+                </a>
+              </li>
+            ))}
           </ul>
         </div>
       </div>
     )
   }
 }
+
+export default FooterBrands
