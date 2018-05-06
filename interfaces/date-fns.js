86b8af390eee540db620256c82a9a83f09ffb1f3
Sam Kvale <skvale@widen.com>
2018-05-05 22:02:16 -0500

    feat(Note): Add Note component

---
+++ interfaces/date-fns.js
@@ -0,0 +1,15 @@
+declare module 'date-fns/difference_in_seconds' {
+  declare function exports(date1: Date, date2: Date): number
+}
+
+declare module 'date-fns/difference_in_minutes' {
+  declare function exports(date1: Date, date2: Date): number
+}
+
+declare module 'date-fns/difference_in_hours' {
+  declare function exports(date1: Date, date2: Date): number
+}
+
+declare module 'date-fns/format' {
+  declare function exports(date: Date, format: string): string
+}
