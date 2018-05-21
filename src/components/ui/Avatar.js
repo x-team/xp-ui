e7bcd8c79d8e8bca2b4d84896b78b5a2484ec4f5
Nick Plekhanov <nickatgit@yandex.com>
2018-05-21 03:24:18 +0300

    Refactor Avatar component and its docs

--- src/components/ui/Avatar.js
+++ src/components/ui/Avatar.js
@@ -1,57 +1,44 @@
+// @flow
+
 import { PureComponent } from 'react'
 
 import elem from '../../utils/elem'
 
-type Props = {
-  src: string,
-  alt: ?string
-}
-
 const cmz = require('cmz')
 
-// Size options
-const sizeStyles = {
-  small: cmz(`
-    width: 32px
-    height: 32px
-  `),
-  normal: cmz(`
-    width: 64px
-    height: 64px
-  `)
+type Props = {
+  alt?: string,
+  src: string,
+  size: number
 }
 
 const Root = elem.div(cmz(`
-  border-radius: 50%
   display: inline-block
+  border-radius: 50%
   overflow: hidden
 `))
 
 const Image = elem.img(cmz(`
-  width: 100%
-  height: 100%
+  max-width: 100%
+  display:block
+  height: auto
 `))
 
-class Avatar extends PureComponent<Props> {
+class Avatar extends PureComponent<Props, void> {
   static defaultProps = {
-    size: 'normal',
+    size: 64,
     alt: ''
   }
 
   render () {
-    const {
-      alt,
-      src,
-      size
-    } = this.props
-
-    const sizeClassName = sizeStyles[size] || ''
+    const { alt, src, size } = this.props
 
     return Root(
-      { className: sizeClassName },
       Image({
         alt,
-        src
+        src,
+        width: size,
+        height: size
       })
     )
   }
