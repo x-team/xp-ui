b712fc473e7b407c76710c30ca2bd61d4a71825f
Nick Plekhanov <nickatgit@yandex.com>
2018-05-27 20:48:13 +0300

    Refactor Loader component

--- src/components/ui/Loader.js
+++ src/components/ui/Loader.js
@@ -1,21 +1,16 @@
 // @flow
 
-import { PureComponent } from 'react'
 import elem from '../../utils/elem'
-
-type Props = {}
 
 const Img = elem.img('', {
   src: require('../../assets/x-loader.gif'),
   alt: 'Loading...'
 })
 
-class Loader extends PureComponent<Props> {
-  render () {
-    return (
-      Img()
-    )
-  }
+function Loader () {
+  return (
+    Img()
+  )
 }
 
 export default Loader
--- src/components/ui/Loader.md
+++ src/components/ui/Loader.md
@@ -1,3 +1,5 @@
+Basic usage:
+
 ```js
 <Loader />
 ```
