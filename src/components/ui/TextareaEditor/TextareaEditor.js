aa02c7ffdcaf3ada38c50021a9cd788a675dc144
Jordan Jones <proxima.aust@gmail.com>
2018-05-29 14:18:37 -0500

    Use classnames instead of inline styles

--- src/components/ui/TextareaEditor/TextareaEditor.js
+++ src/components/ui/TextareaEditor/TextareaEditor.js
@@ -78,7 +78,6 @@ type Props = {
   html: string,
   id: string,
   charLimit: number,
-  large: number,
   onChange(text: string, html: string): ?void,
   onFocus(target: Object, text: string, html: string): ?void,
   onBlur(target: Object, text: string, html: string): ?void,
