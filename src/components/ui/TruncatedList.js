020d3f69cac0c6ff5035f32d11e98aedee353caa
Nick Plekhanov <nickatgit@yandex.com>
2018-06-30 01:57:48 +0300

    Code refactors

--- src/components/ui/TruncatedList.js
+++ src/components/ui/TruncatedList.js
@@ -4,16 +4,18 @@ import React, { PureComponent } from 'react'
 
 const cmz = require('cmz')
 
-const styles = {
+const cx = {
   list: cmz(`
     list-style: none
     margin: 0
     padding: 0
   `),
+
   item: cmz(`
     display: inline-block
     padding: 0
   `),
+
   hidden: cmz(`
     display: none
   `)
@@ -55,13 +57,14 @@ class TruncatedList extends PureComponent<Props, State> {
     }
   }
 
-  getInitialState = (prevState: State | Object, props: Props) => {
+  getInitialState = ({ allVisible, page }: State | Object, props: Props) => {
     const { items, visible, increment, inserted } = props
     const hiddenAmount = items.length >= visible ? items.length - visible : 0
+
     return {
-      allVisible: prevState.allVisible || hiddenAmount === 0,
+      allVisible: allVisible || hiddenAmount === 0,
       hiddenItems: inserted ? hiddenAmount + 1 : hiddenAmount,
-      page: prevState.page || 1,
+      page: page || 1,
       pagesCount: increment ? hiddenAmount / increment + 1 : 2,
       itemsLength: items.length
     }
@@ -69,6 +72,7 @@ class TruncatedList extends PureComponent<Props, State> {
 
   handleViewMore = () => {
     const { visible, increment, inserted } = this.props
+
     this.setState((prevState: State) => ({
       allVisible: increment
         ? prevState.page + 1 >= prevState.pagesCount
@@ -100,7 +104,7 @@ class TruncatedList extends PureComponent<Props, State> {
         ? viewMore(nextRealIncrement, this.handleViewMore)
         : (
           <li
-            className={styles.item}
+            className={cx.item}
             onClick={this.handleViewMore}
           >
             {increment
@@ -112,12 +116,12 @@ class TruncatedList extends PureComponent<Props, State> {
     }
 
     return itemsLength > 0 ? (
-      <ul className={styles.list}>
+      <ul className={cx.list}>
         {items
           .map((item, i) => {
             const isVisible = allVisible || i < realVisible || (increment && i < nextView)
-              ? styles.item
-              : [styles.item, styles.hidden].join(' ')
+              ? cx.item
+              : [cx.item, cx.hidden].join(' ')
             return (
               <li className={isVisible} key={i}>
                 {item}
