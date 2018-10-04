6ee84e64901c181f2c448178b5eaa393bbf8d1a2
Ronald Rey <reyronald@gmail.com>
2018-10-04 18:08:26 -0400

    Stretch divider line below "Select List" input (#234)
    * Stretch divider line below "Select List" input
    
    This stretches the divider line between the "Select List" input
    and the rest of the inputs in the SearchForm component.
    
    Closes XP-2284
    
    * Update `SearchForm` component snapshots
    
    * npm run build
    
    * Remove empty declaration
    
    * Fix Keywords select's suggestions width and position
    
    * Update snapshots

--- src/components/ui/SearchForm.js
+++ src/components/ui/SearchForm.js
@@ -27,6 +27,7 @@ const listTheme = {
   selectLists: cmz(`
     display: flex
     align-items: center
+    padding: 30px 30px 0
   `),
 
   listsSelector: cmz(`
@@ -50,17 +51,30 @@ const listTheme = {
   `),
 
   formKeywords: cmz(`
-    display: block
-    width: 100%
-    margin-top: 20px
+   & {
+     display: block
+     width: 100%
+     margin-top: 20px
+     padding: 0 30px
+    }
+
+    & .Select-menu-outer {
+      width: calc(100% - 60px)
+      left: 30px
+    }
   `),
 
   selectFields: cmz(`
     display: inline-block
     width: 100%
     margin: 20px 0 0
-    padding: 20px 0 0
+    padding: 20px 30px 0
     border-top: 1px solid ${theme.lineSilver4}
+    box-sizing: border-box
+  `),
+
+  formButtonContainer: cmz(`
+    padding: 0 30px
   `),
 
   formButton: cmz(`
@@ -75,7 +89,7 @@ const listTheme = {
   applicantsStatusFilter: cmz(`
     width: 100%
     background-color: ${theme.baseBright}
-    padding: 20px 10px 0
+    padding: 20px 30px 0
     box-sizing: border-box
   `)
 }
@@ -209,7 +223,7 @@ class SearchForm extends PureComponent<Props> {
     } = this.props
 
     const isTabular = mode === DISPLAY_MODES.TABULAR
-    const themeClasses = isTabular ? tabularTheme : listTheme
+    const themeClasses: Object = isTabular ? tabularTheme : listTheme
 
     const renderDisplaySwitchButtons = () => (
       <div className={themeClasses.displayButtons}>
@@ -277,13 +291,15 @@ class SearchForm extends PureComponent<Props> {
             onSubmit={onSubmit}
             className={themeClasses.formKeywords}
           />
-          <Button
-            className={themeClasses.formButton}
-            type='submit'
-            raised
-          >
-            Show
-          </Button>
+          <div className={themeClasses.formButtonContainer}>
+            <Button
+              className={themeClasses.formButton}
+              type='submit'
+              raised
+            >
+              Show
+            </Button>
+          </div>
           {isTabular && renderDisplaySwitchButtons()}
         </form>
         {renderApplicantsStatusFilter && (
