d38d9f4e09c978690f4981a14f0a686c014de4a2
carlosalmonter <carlosalmonter@gmail.com>
2018-10-08 16:50:59 -0400

    Remove Milestone component

--- src/components/ui/MilestonesScreen.js
+++ src/components/ui/MilestonesScreen.js
@@ -1,10 +1,8 @@
 // @flow
 
-import React, { PureComponent, Children } from 'react'
+import { PureComponent, Children } from 'react'
 
 import elem from '../../utils/elem'
-
-import Milestones from './Milestones'
 
 import theme, { breakpoints } from '../../styles/theme'
 
@@ -13,19 +11,8 @@ import type { Element } from 'react'
 const cmz = require('cmz')
 
 type Props = {
-  level: number,
   children?: Element<*>,
-  cta?: Element<*>,
-  hideMilestones?: boolean
-}
-
-const cx = {
-  hiddenMilestones: cmz(`
-    & > *:only-child,
-    & > *:first-child {
-      padding-top: 0 !important
-    }
-  `)
+  cta?: Element<*>
 }
 
 const Root = elem.div()
@@ -59,6 +46,7 @@ const Content = elem.div(cmz(`
   & > *:only-child,
   & > *:first-child {
     border-top: none !important
+    padding-top: 0 !important
   }
 
   & > *:only-child,
@@ -76,38 +64,19 @@ const Block = elem.div(cmz(`
 
 class MilestonesScreen extends PureComponent<Props> {
   static defaultProps = {
-    level: 0,
     children: null,
-    cta: null,
-    hideMilestones: false
+    cta: null
   }
 
   render () {
-    const { level, children, cta, hideMilestones } = this.props
-
-    const milestones = level > 0 && !hideMilestones && (
-      <Milestones
-        level={level}
-        levels={[
-          { icon: 'cog' },
-          { icon: 'message' },
-          { icon: 'webcam' }
-        ]}
-      />
-    )
+    const { children, cta } = this.props
     const childBlocks = Children.map(children, (child, index) => Block(
       { key: index },
       child,
       CTA(cta)
     ))
 
-    return Root(
-      milestones,
-      Wrapper(Content(
-        { className: hideMilestones && cx.hiddenMilestones },
-        childBlocks
-      ))
-    )
+    return Root(Wrapper(Content(childBlocks)))
   }
 }
 
--- src/components/ui/MilestonesScreen.md
+++ src/components/ui/MilestonesScreen.md
@@ -2,7 +2,6 @@ First step:
 
 ```js
 <MilestonesScreen
-  level={1}
   cta={<Button>Go to the step 2</Button>}
 >
   <Text
@@ -16,7 +15,6 @@ Second step:
 
 ```js
 <MilestonesScreen
-  level={2}
   cta={<Button>Go to the step 3</Button>}
 >
   <Text
@@ -35,32 +33,7 @@ N-th step:
 
 ```js
 <MilestonesScreen
-  level={3}
   cta={<Button>Go to the step N+1</Button>}
->
-  <Text
-    heading="First subsection"
-    content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. At suscipit dicta aliquam sint libero minima officiis alias ducimus. Fugit praesentium itaque, iure molestias aut beatae odit facilis inventore doloremque eaque."
-  />
-
-  <Text
-    heading="Second subsection"
-    content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempora neque iusto quisquam, sed eius optio. Obcaecati, hic! Possimus sit magnam vero laboriosam obcaecati nostrum a molestiae amet aut. Corrupti, voluptas."
-  />
-
-  <Text
-    heading="Third subsection"
-    content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit minima aperiam dolorem natus repudiandae reprehenderit neque laudantium quasi voluptates architecto, ratione asperiores odio hic rerum alias dolores quo sint culpa!"
-  />
-</MilestonesScreen>
-```
-
-Hidden Milestone Icons
-```js
-<MilestonesScreen
-  level={3}
-  cta={<Button>Go to the step N+1</Button>}
-  hideMilestones
 >
   <Text
     heading="First subsection"
