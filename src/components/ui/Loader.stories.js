c01d72afce59d0984a16891812b917e90344b566
Ronald Rey <reyronald@gmail.com>
2018-11-19 10:25:03 -0400

    Write stories for Loader

---
+++ src/components/ui/Loader.stories.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+
+import Loader from './Loader'
+
+storiesOf('UI Components/Loader', module).add('basic usage', () => <Loader />)
