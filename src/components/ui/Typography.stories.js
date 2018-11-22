83d3212d5ac2b7314ec0ce26aaa8d3dfdaf199d9
Ronald Rey <reyronald@gmail.com>
2018-11-22 11:13:48 -0400

    Write stories for Typography

---
+++ src/components/ui/Typography.stories.js
@@ -0,0 +1,12 @@
+// @flow
+
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+
+import Typography from './Typography'
+
+storiesOf('UI Components/Typography', module)
+  .add('typography sets for development reference', () => (
+    <Typography />
+
+  ))
