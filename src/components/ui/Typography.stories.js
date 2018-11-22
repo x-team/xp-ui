b607204ed1df33cf33c3964d2984d61b3511bce3
Nick Plekhanov <nickatgit@yandex.com>
2018-11-22 19:13:50 +0300

    One line change :)

--- src/components/ui/Typography.stories.js
+++ src/components/ui/Typography.stories.js
@@ -6,7 +6,4 @@ import { storiesOf } from '@storybook/react'
 import Typography from './Typography'
 
 storiesOf('UI Components/Typography', module)
-  .add('typography sets for development reference', () => (
-    <Typography />
-
-  ))
+  .add('typography sets for development reference', () => <Typography />)
