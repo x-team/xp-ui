700355639bc7283f00f6690b7742764f3ae9141c
Bernardo Dias <bernardodiasdacruz@gmail.com>
2018-12-06 19:12:38 -0200

    Mock faker library

---
+++ __mocks__/faker.js
@@ -0,0 +1,16 @@
+// @flow
+
+module.exports = {
+  random: {
+    number: () => 0,
+    words: () => '',
+    word: () => ''
+  },
+  name: {
+    firstName: () => '',
+    lastName: () => ''
+  },
+  date: {
+    month: () => ''
+  }
+}
