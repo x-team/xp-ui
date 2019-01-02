16c314923795422e6fcfbedda26e49ec13859444
Ronald Rey <reyronald@gmail.com>
2019-01-02 12:18:12 -0400

    Fix Markdown mode by adding CodeMirror styles

--- src/components/ui/RichTextEditor.js
+++ src/components/ui/RichTextEditor.js
@@ -8,13 +8,19 @@ import typo from '../../styles/typo'
 
 const cmz = require('cmz')
 
+require('codemirror/lib/codemirror.css') // codemirror
 require('tui-editor/dist/tui-editor.css') // editor ui
 require('tui-editor/dist/tui-editor-contents.css') // editor content
+require('highlight.js/styles/github.css') // code block highlight
 
 const cx = {
   root: cmz(`
     & * {
       box-sizing: border-box;
+    }
+
+    & .CodeMirror-scroll {
+      overflow-x: hidden !important;
     }
   `),
 
