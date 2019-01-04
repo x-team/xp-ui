850088856ecb401e0f35ee24fac7b077cb165e43
Ronald Rey <reyronald@gmail.com>
2019-01-04 16:39:18 -0400

    Implement generic Rich Text Editor component (#315)
    * Create configurable component that implements ToastUI text editor
    
    Closes XP-2672
    
    * Snapshots + Built lib
    
    * Fix Markdown mode by adding CodeMirror styles
    
    * Fix Flow build by adding libdefs
    
    * Update snapshots + Build ilb
    
    * Use single `handleChange` prop
    
    * Update package-lock.json
    
    * Tiny Editor module updates

---
+++ flow-typed/npm/codemirror_vx.x.x.js
@@ -0,0 +1,6 @@
+// flow-typed signature: 3f72b9898dccd091fe1967e7fa04d641
+// flow-typed version: <<STUB>>/codemirror_v5.42.2/flow_v0.61.0
+
+declare module 'codemirror/lib/codemirror.css' {
+  declare module.exports: any;
+}
