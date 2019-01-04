51bf1b17b46cf1e829a27ba16f49834d54cf6384
Ronald Rey <reyronald@gmail.com>
2019-01-04 16:39:18 -0400

    Implement generic Rich Text Editor component (#315)
    * Create configurable component that implements ToastUI text editor
    
    Closes XP-2672
    
    * Snapshots + Built lib
    
    * Fix Markdown mode by adding CodeMirror styles
    
    * Fix Flow build by adding libdefs
    
    * Update snapshots + Build ilb
    
    * Use single `handleChange` prop
    
    * Update package-lock.json
    
    * Tiny Editor module updates

---
+++ flow-typed/npm/highlight.js_vx.x.x.js
@@ -0,0 +1,6 @@
+// flow-typed signature: cbade3e3fc9726e8f740cec7c97ea77f
+// flow-typed version: <<STUB>>/highlight.js_v9.13.1/flow_v0.61.0
+
+declare module 'highlight.js/styles/github.css' {
+  declare module.exports: any;
+}
---
+++ src/components/ui/RichTextEditor.js
@@ -0,0 +1,135 @@
+// @flow
+/* global HTMLDivElement */
+
+import React, { Component } from 'react'
+import Editor from 'tui-editor'
+
+import typo from '../../styles/typo'
+
+const cmz = require('cmz')
+
+require('codemirror/lib/codemirror.css') // codemirror
+require('tui-editor/dist/tui-editor.css') // editor ui
+require('tui-editor/dist/tui-editor-contents.css') // editor content
+require('highlight.js/styles/github.css') // code block highlight
+
+const cx = {
+  root: cmz(`
+    & * {
+      box-sizing: border-box;
+    }
+
+    & .CodeMirror-scroll {
+      overflow-x: hidden !important;
+    }
+  `),
+
+  characterCounter: cmz(
+    typo.baseText,
+    `
+    margin-left: 10px;
+    font-size: 12px;
+    line-height: 20px;
+  `)
+}
+
+type Props = {
+  initialValue?: string,
+  characterLimit: number,
+  hideModeSwitch: boolean,
+  handleChange({ html: string, markdown: string, plainText: string }): void
+}
+
+type State = {
+  characterCount: number
+}
+
+class RichTextEditor extends Component<Props, State> {
+  static defaultProps = {
+    characterLimit: Infinity,
+    hideModeSwitch: true,
+    handleChange: () => {}
+  }
+
+  state = {
+    characterCount: 0
+  }
+
+  // $FlowFixMe: Local version of flow is out-dated and doesn't have definitions for createRef
+  editSection = React.createRef()
+  editor: *
+  editorContentsNode: HTMLDivElement
+  prevValue: string = ''
+
+  componentDidMount () {
+    const { initialValue, hideModeSwitch } = this.props
+
+    if (this.editSection.current) {
+      this.editor = new Editor({
+        el: this.editSection.current,
+        initialValue,
+        hideModeSwitch,
+        initialEditType: 'wysiwyg',
+        previewStyle: 'vertical',
+        height: 'auto',
+        usageStatistics: false,
+        events: { change: this.onChange }
+      })
+
+      this.editorContentsNode = this.editSection.current.querySelector(
+        '.tui-editor-contents[contenteditable=true]'
+      )
+
+      if (initialValue) {
+        this.prevValue = this.editor.getValue()
+        const characterCount = (this.editorContentsNode.innerText || '').trim().length
+        this.setState({ characterCount })
+      }
+    }
+  }
+
+  componentDidUpdate (prevProps: Props) {
+    if (prevProps.hideModeSwitch !== this.props.hideModeSwitch) {
+      this.props.hideModeSwitch
+        ? this.editor.getUI().getModeSwitch().hide()
+        : this.editor.getUI().getModeSwitch().show()
+    }
+
+    if (prevProps.initialValue !== this.props.initialValue) {
+      this.editor.setValue(this.props.initialValue)
+    }
+  }
+
+  onChange = () => {
+    const { characterLimit, handleChange } = this.props
+    const values = {
+      markdown: this.editor.getMarkdown(),
+      html: this.editor.getHtml(),
+      plainText: this.editorContentsNode.innerText || ''
+    }
+
+    const characterCount = values.plainText.trim().length
+
+    if (characterCount > characterLimit && this.prevValue.length < this.editor.getValue().length) {
+      this.editor.setValue(this.prevValue)
+    } else {
+      handleChange(values)
+      this.prevValue = this.editor.getValue()
+      this.setState({ characterCount })
+    }
+  }
+
+  render () {
+    const { characterCount } = this.state
+    const { characterLimit } = this.props
+
+    return <div className={cx.root}>
+      <div ref={this.editSection} />
+      {characterLimit !== Infinity && (
+        <div className={cx.characterCounter}>{characterCount}/{characterLimit}</div>
+      )}
+    </div>
+  }
+}
+
+export default RichTextEditor
