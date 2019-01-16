c91d4243c154cc2eaf5ea57137dc24df47a2da7a
Kamil Ronewicz <galileox86@gmail.com>
2019-01-16 03:19:39 -0800

    Update top profile design (#325)
    * Upgrade button and SvgIcon component to adapt new style on auto (#322)
    
    * Upgrade button and Svg component to adapt new style on auto
    
    * Add new props to button component: icon, iconProps and contentStyle
    
    * Add new color to Svgicon and new custom style for button components
    
    * Build app
    
    *  Add Activity Header (#324)
    
    * Add time SvgIcon
    
    * Add Activity Header
    
    * Fix flexbox for grid
    
    * Add build
    
    * Address PR requested changes
    
    * Fix activity description `line-height` + remove `min-width`
    
    * Remove `max-width`
    
    * Use `rem` instead of `px` fot font-size + Fix snapshots
    
    * Update padding button fro sourceSansPro content style
    
    * Build app
    
    # Conflicts:
    #	lib/auto-ui.js.map

---
+++ src/components/ui/ActivityBar.js
@@ -0,0 +1,57 @@
+// @flow
+
+import React from 'react'
+import SvgIcon from './SvgIcon'
+
+import { timeSince } from '../../utils/helpers'
+import theme from '../../styles/theme'
+
+const cmz = require('cmz')
+
+const cx = {
+  container: cmz(`
+    & {
+      flex: 1 0 100%
+      flex-flow: row
+      display: flex
+      align-items: center
+      font-family: Source Sans Pro
+      font-weight: 400
+      font-size: 0.875rem
+      line-height: 1
+    }
+    
+    & > * {
+      flex-shrink: 0
+    }
+  `),
+
+  activity: cmz(`
+    line-height: 1.57
+    flex-shrink: 1
+    padding: 0 8px
+   
+    white-space: nowrap
+    overflow:hidden
+    text-overflow: ellipsis
+  `),
+
+  time: cmz(`
+    padding: 1px 8px
+    color: ${theme.typoLabel}
+    border-left: 1px solid ${theme.silver}
+  `)
+}
+
+type Props = {
+  text?: string,
+  datetime?: Date | string | number
+}
+
+export default function ActivityBar ({ text, datetime }: Props) {
+  return <div className={cx.container}>
+    <SvgIcon icon='time' color='grayscale' />
+    <div className={cx.activity} title={text}>{text}</div>
+    <div className={cx.time}>{timeSince(datetime, false)}</div>
+  </div>
+}
