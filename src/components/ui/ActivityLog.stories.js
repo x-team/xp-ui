981c0f48b28358050ebfa5306229e6fd63c04c16
Bernardo Dias <bernardodiasdacruz@gmail.com>
2019-01-21 13:27:54 -0200

    Update ActivityLogsDisplay (#328)
    * Update ActivityLogsDisplay
    
    * Fix build
    
    * Update stories for ActivityLogs and ActivityLogsDisplay
    
    * Include extra info prop to be displayed at ActivityLog
    
    * Fix build
    
    * Prevent unecessary colons at ActivityLog displays
    
    # Conflicts:
    #	lib/auto-ui.js.map

--- src/components/ui/ActivityLog.stories.js
+++ src/components/ui/ActivityLog.stories.js
@@ -5,13 +5,23 @@ import { storiesOf } from '@storybook/react'
 import ActivityLog from './ActivityLog'
 
 storiesOf('UI Components/ActivityLogsDisplay/ActivityLog', module)
-  .add('single value', () => (
+  .add('single activity with value and label', () => (
     <ActivityLog
       label={faker.random.words(2)}
       value={faker.random.words(2)}
     />
   ))
-  .add('grouped values', () => (
+  .add('single activity with only value', () => (
+    <ActivityLog
+      value={faker.random.words(2)}
+    />
+  ))
+  .add('single activity with only label', () => (
+    <ActivityLog
+      label={faker.random.words(2)}
+    />
+  ))
+  .add('grouped activities', () => (
     <ActivityLog
       label={faker.random.words(2)}
       value={Array(faker.random.number(3))
@@ -21,6 +31,32 @@ storiesOf('UI Components/ActivityLogsDisplay/ActivityLog', module)
           value: faker.random.words(2)
         }))
       }
+    />
+  ))
+  .add('grouped activities with extra details on elements elements', () => (
+    <ActivityLog
+      label={faker.random.words(2)}
+      value={Array(faker.random.number(3))
+        .fill({})
+        .map(() => ({
+          label: faker.random.words(2),
+          value: faker.random.words(2),
+          details: faker.random.words(5)
+        }))
+      }
+    />
+  ))
+  .add('grouped activities with customised group values', () => (
+    <ActivityLog
+      label={faker.random.words(2)}
+      value={Array(faker.random.number(3))
+        .fill({})
+        .map(() => ({
+          label: faker.random.words(2),
+          value: faker.random.words(2)
+        }))
+      }
+      groupValues={faker.random.words(2)}
     />
   ))
   .add('missing props (does component explode?)', () => (
