c600a222cbea66cb91afb8857f0ea63eb918f84e
Bernardo Dias <bernardodiasdacruz@gmail.com>
2019-02-20 12:12:27 -0300

    Rename Tab to GenericTabs and Collapsible to GenericCollapsible (#353)
    * Rename Tab to GenericTabs
    
    * Rename Collapsible to GenericCollapsible
    
    * Update snapshots

--- src/components/ui/Filters/Filter.js
+++ src/components/ui/Filters/Filter.js
@@ -3,6 +3,8 @@
 import React, { Fragment } from 'react'
 
 import Label from './Label'
+import GenericCollapsible from '../GenericCollapsible'
+import SvgIcon from '../SvgIcon'
 
 import type { Element } from 'react'
 
@@ -19,7 +21,24 @@ const cx = {
   `)
 }
 
-const Filter = (props: Props) => (
+const Filter = (props: Props) => props.isCollapsible ? (
+  <GenericCollapsible.Container initialExpanded>
+    <GenericCollapsible.Header>
+      <div className={cx.header}>
+        <Label>{props.label}</Label>
+        <div className={cx.triangle}>
+          <SvgIcon
+            icon={props.isExpanded ? 'triangleup' : 'triangledown'}
+            color='grayscarpaflow'
+          />
+        </div>
+      </div>
+    </GenericCollapsible.Header>
+    <GenericCollapsible.Body>
+      <div className={cx.filter}>{props.children}</div>
+    </GenericCollapsible.Body>
+  </GenericCollapsible.Container>
+) : (
   <Fragment>
     <Label>{props.label}</Label>
     <div className={cx.filter}>{props.children}</div>
