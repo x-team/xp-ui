6d9331c594bd8b7ae997899585b6f0c8f4357df4
Bernardo Dias <bernardodiasdacruz@gmail.com>
2019-02-27 17:28:40 -0300

    Remove isCollapsible behaviour entirely from Filters.Filter component (#358)
    * Expose existing bug in Storybook with Filter component
    
    * Remove isCollapsible behaviour entirely from Filters.Filter component
    
    * Build app
    
    # Conflicts:
    #	lib/xp-ui.js.map

--- src/components/ui/Filters/Filter.js
+++ src/components/ui/Filters/Filter.js
@@ -3,15 +3,12 @@
 import React, { Fragment } from 'react'
 
 import Label from './Label'
-import GenericCollapsible from '../GenericCollapsible'
-import SvgIcon from '../SvgIcon'
 
 import type { Element } from 'react'
 
 export type Props = {
   label: Element<*>,
-  children: Element<*>,
-  isCollapsible: boolean
+  children: Element<*>
 }
 
 const cmz = require('cmz')
@@ -19,36 +16,10 @@ const cmz = require('cmz')
 const cx = {
   filter: cmz(`
     padding: 0 60px 20px
-  `),
-
-  header: cmz(`
-    display: flex
-    cursor: pointer
-  `),
-
-  triangle: cmz(`
-    padding: 0 60px 10px 0
   `)
 }
 
-const Filter = (props: Props) => props.isCollapsible ? (
-  <GenericCollapsible.Container initialExpanded>
-    <GenericCollapsible.Header>
-      <div className={cx.header}>
-        <Label>{props.label}</Label>
-        <div className={cx.triangle}>
-          <SvgIcon
-            icon={props.isExpanded ? 'triangleup' : 'triangledown'}
-            color='grayscarpaflow'
-          />
-        </div>
-      </div>
-    </GenericCollapsible.Header>
-    <GenericCollapsible.Body>
-      <div className={cx.filter}>{props.children}</div>
-    </GenericCollapsible.Body>
-  </GenericCollapsible.Container>
-) : (
+const Filter = (props: Props) => (
   <Fragment>
     <Label>{props.label}</Label>
     <div className={cx.filter}>{props.children}</div>
