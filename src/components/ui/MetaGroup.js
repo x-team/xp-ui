a66963eed0830f91583a987c675bc4b409eac469
Bernardo Dias <bernardodiasdacruz@gmail.com>
2019-03-08 09:44:45 -0300

    Create MetaGroup UI component (#362)
    * Create MetaGroup component
    
    * Update MetaGroup showcase
    
    * Export MetaGroup component
    
    * Use different element.key for the mapped elements key
    
    * Rename MetaGroup props
    
    # Conflicts:
    #	lib/xp-ui.js.map

---
+++ src/components/ui/MetaGroup.js
@@ -0,0 +1,67 @@
+// @flow
+
+import React, { PureComponent } from 'react'
+
+import type { Element } from 'react'
+
+const cmz = require('cmz')
+
+const cx = {
+  layout: cmz(`
+    position: relative
+    display: flex
+    height: inherit
+  `),
+
+  mainBodyColumn: cmz(`
+    box-sizing: border-box
+    flex: 1
+    min-width: 60%
+  `),
+
+  secondaryColumns: cmz(`
+    display: flex
+    flex-wrap: wrap
+    justify-content: flex-end
+    align-items: flex-start
+    height: 100%
+    width: 300px
+  `),
+
+  secondaryColumn: cmz(`
+    box-sizing: border-box
+    flex-shrink: 0
+  `)
+}
+
+type Props = {
+  mainBodyElement: Element<*>,
+  secondaryElements: Array<Element<*>>,
+}
+
+class MetaGroup extends PureComponent<Props, void> {
+  static defaultProps = {
+    mainBodyElement: null,
+    secondaryElements: []
+  }
+
+  render () {
+    const { mainBodyElement, secondaryElements } = this.props
+    return (
+      <div className={cx.layout}>
+        <div className={cx.mainBodyColumn}>
+          {mainBodyElement}
+        </div>
+        <div className={cx.secondaryColumns}>
+          {secondaryElements.map(element => (
+            <div className={cx.secondaryColumn} key={element.key}>
+              {element}
+            </div>
+          ))}
+        </div>
+      </div>
+    )
+  }
+}
+
+export default MetaGroup
