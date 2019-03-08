11d102aefeb7462b3fd66d87fda20564db5e5e32
Miguel Angel Liriano <mglphp@gmail.com>
2019-03-08 11:39:47 -0400

    Create UI component to show result number of elements (#364)
    * Create MetaGroup component
    
    * Update MetaGroup showcase
    
    * Export MetaGroup component
    
    * Use different element.key for the mapped elements key
    
    * Create ui component to show result number of element
    
    * Remove object count
    
    * Build app
    
    * Add singular and plural suffix to text
    
    * Only use number type for result count
    
    * Update snapshots
    
    * Refactor the component
    
    # Conflicts:
    #	lib/xp-ui.js.map
    #	package-lock.json

---
+++ src/components/ui/ResultCount.stories.js
@@ -0,0 +1,11 @@
+// @flow
+
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+
+import ResultCount from './ResultCount'
+
+storiesOf('UI Components/ResultCount', module)
+  .add('basic usage', () => (<ResultCount items={15} />))
+  .add('usage with single item result', () => (<ResultCount items={1} />))
+  .add('missing props (does component explode?)', () => (<ResultCount />))
