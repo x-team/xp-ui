d1d5255e8c7666c305ac1cfc968996b0f854a625
Miguel Angel Liriano <mglphp@gmail.com>
2019-03-08 11:39:47 -0400

    Create UI component to show result number of elements (#364)
    * Create MetaGroup component
    
    * Update MetaGroup showcase
    
    * Export MetaGroup component
    
    * Use different element.key for the mapped elements key
    
    * Create ui component to show result number of element
    
    * Remove object count
    
    * Build app
    
    * Add singular and plural suffix to text
    
    * Only use number type for result count
    
    * Update snapshots
    
    * Refactor the component

---
+++ src/components/ui/ResultCount.js
@@ -0,0 +1,35 @@
+// @flow
+
+import React from 'react'
+import isNumber from 'lodash.isnumber'
+
+const cmz = require('cmz')
+
+const cx = {
+  wrapper: cmz(`
+    font-size: 1.0625rem;
+  `),
+
+  content: cmz(`
+    font-weight: 600;
+    margin-right: 5px;
+  `)
+}
+
+type Props = {
+  items?: number
+}
+
+const ResultCount = ({ items }: Props) => {
+  const itemsCount = isNumber(items) ? items : 0
+  const resultText = parseInt(itemsCount, 10) !== 1 ? 'results' : 'result'
+
+  return (
+    <span className={cx.wrapper}>
+      <span className={cx.content}>{itemsCount}</span>
+      {resultText}
+    </span>
+  )
+}
+
+export default ResultCount
