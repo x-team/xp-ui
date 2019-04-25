31485ebc140b8063a32c704407593873c3bc4ffc
Bernardo Emilio Rubin de Celis Martinez <bernulander@gmail.com>
2019-04-25 13:55:38 -0500

    Remove add-on options

--- .storybook/addons.js
+++ .storybook/addons.js
@@ -7,9 +7,6 @@ import '@storybook/addon-viewport/register'
 // Changes background colors inside the preview
 import '@storybook/addon-backgrounds/register'
 
-// (Re-)configures the Storybook UI at runtime
-import '@storybook/addon-options/register'
-
 // Allows to write notes (text or HTML) for our stories
 import '@storybook/addon-notes/register'
 
