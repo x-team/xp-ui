d424fa89bd18e929a11d42aeed04cc00469d13d3
Carlos Almeida <carlosalmeida.xon@gmail.com>
2019-07-04 00:53:15 -0400

    added lazy-load to the Avatar

--- src/components/ui/Avatar.js
+++ src/components/ui/Avatar.js
@@ -1,40 +1,54 @@
 // @flow
-import React from 'react'
-import ReactAvatar from 'react-avatar'
+import React from 'react';
+import ReactAvatar from 'react-avatar';
+import VisibilitySensor from 'react-visibility-sensor';
 
-import theme from '../../styles/theme'
+import theme from '../../styles/theme';
 
-const cmz = require('cmz')
+const cmz = require('cmz');
 
 const cx = {
   avatarSizeConstraint: cmz(`
     max-width: 100%
     max-height: 100%
   `)
-}
+};
 
 type Props = {
   alt?: string,
   src: string,
   size: number
-}
+};
 
 const Avatar = (props: Props) => {
-  const { alt, ...rest } = props
+  const { alt, src, size } = props;
+  let imgSrc = null;
 
-  return <ReactAvatar
-    className={cx.avatarSizeConstraint}
-    name={alt}
-    color={theme.baseRed.toString()}
-    maxInitials={3}
-    round
-    {...rest}
-  />
-}
+  return (
+    <VisibilitySensor>
+      {({ isVisible }) => {
+        if (isVisible) imgSrc = src;
+
+        return (
+          <ReactAvatar
+            className={cx.avatarSizeConstraint}
+            name={alt}
+            color={theme.baseRed.toString()}
+            maxInitials={3}
+            alt={alt}
+            src={imgSrc}
+            size={size}
+            round
+          />
+        );
+      }}
+    </VisibilitySensor>
+  );
+};
 
 Avatar.defaultProps = {
   size: 64,
   alt: ''
-}
+};
 
-export default Avatar
+export default Avatar;
