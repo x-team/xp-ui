d63d7b53b4d1d84b2baac3da0b5a76375850446e
Josh Johnston <josh@x-team.com>
2019-08-06 18:05:26 +0100

    set up multiple outputs

---
+++ src/styles/fonts.js
@@ -0,0 +1,11 @@
+import WebFont from 'webfontloader'
+
+WebFont.load({
+  google: {
+    families: [
+      'Open Sans:400,600,700,800',
+      'Source Sans Pro:300,700',
+      'Montserrat:400'
+    ]
+  }
+})
