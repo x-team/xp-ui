0eeaef17643d105aa52aa7425bef09dd01f80a7a
Bernardo Dias <bernardodiasdacruz@gmail.com>
2019-09-05 11:49:29 -0300

    Remove unused UI components (#430)
    * Remove unused UI components
    
    * Include build
    
    # Conflicts:
    #	lib/all.js
    #	lib/editor.js
    #	lib/editor.js.map
    #	lib/fonts.js
    #	lib/fonts.js.map
    #	lib/registration.js
    #	lib/registration.js.map
    #	lib/xp-ui.js.map
    #	src/__snapshots__/storyshots.test.js.snap
    #	src/registration.js

--- src/index.js
+++ src/index.js
@@ -8,7 +8,6 @@ import ApplicantScreen from './components/ui/ApplicantScreen'
 import AttachFiles from './components/ui/AttachFiles'
 import Avatar from './components/ui/Avatar'
 import Button from './components/ui/Button'
-import CollapsibleSection from './components/ui/CollapsibleSection'
 import ColorPalette from './components/ui/ColorPalette'
 import ColumnsCustomizer from './components/ui/ColumnsCustomizer'
 import DataGrid from './components/ui/DataGrid'
@@ -17,7 +16,6 @@ import Email from './components/ui/Email'
 import EmailFeed from './components/ui/EmailFeed'
 import ErrorBox from './components/ui/ErrorBox'
 import Filters from './components/ui/Filters'
-import FilterTag from './components/ui/FilterTag'
 import Footer from './components/ui/Footer/Footer'
 import FooterBrands from './components/ui/Footer/FooterBrands'
 import FooterList from './components/ui/Footer/FooterList'
@@ -31,12 +29,10 @@ import InlineEditor from './components/ui/InlineEditor'
 import InputField from './components/forms/InputField'
 import InputGroup from './components/forms/InputGroup'
 import IssueCollectorButton from './components/ui/IssueCollectorButton'
-import Keywords from './components/ui/Keywords'
 import ListExclusionFormPopup from './components/ui/ListExclusionFormPopup'
 import ListsEditor from './components/ui/ListsEditor'
 import Loader from './components/ui/Loader'
 import MetaGroup from './components/ui/MetaGroup'
-import Milestones from './components/ui/Milestones'
 import MilestonesScreen from './components/ui/MilestonesScreen'
 import Modal from './components/ui/Modal'
 import Note from './components/ui/Note'
@@ -46,16 +42,11 @@ import ProfileHeaderLinks from './components/ui/ProfileHeaderLinks'
 import QuickSearchCloseButton from './components/ui/QuickSearchCloseButton'
 import ResultCount from './components/ui/ResultCount'
 import RichTextEditor from './components/ui/RichTextEditor'
-import Roadmap from './components/ui/Roadmap'
 import RoadmapHero from './components/ui/RoadmapHero'
-import RoadmapLevel from './components/ui/RoadmapLevel'
-import RoadmapTimelineElement from './components/ui/RoadmapTimelineElement'
-import SearchForm from './components/ui/SearchForm'
 import SearchResult from './components/ui/SearchResult'
 import SelectBox from './components/ui/SelectBox'
 import SettingsImportScreen from './components/ui/SettingsImportScreen'
 import SettingsScreen from './components/ui/SettingsScreen'
-import SolutionForm from './components/forms/SolutionForm'
 import SvgIcon from './components/ui/SvgIcon'
 import Tab from './components/ui/Tabs/Tab'
 import Tabs from './components/ui/Tabs/Tabs'
@@ -78,7 +69,6 @@ export {
   AttachFiles,
   Avatar,
   Button,
-  CollapsibleSection,
   ColorPalette,
   ColumnsCustomizer,
   DataGrid,
@@ -87,7 +77,6 @@ export {
   EmailFeed,
   ErrorBox,
   Filters,
-  FilterTag,
   Footer,
   FooterBrands,
   FooterList,
@@ -101,12 +90,10 @@ export {
   InputField,
   InputGroup,
   IssueCollectorButton,
-  Keywords,
   ListExclusionFormPopup,
   ListsEditor,
   Loader,
   MetaGroup,
-  Milestones,
   MilestonesScreen,
   Modal,
   Note,
@@ -116,16 +103,11 @@ export {
   QuickSearchCloseButton,
   ResultCount,
   RichTextEditor,
-  Roadmap,
   RoadmapHero,
-  RoadmapLevel,
-  RoadmapTimelineElement,
-  SearchForm,
   SearchResult,
   SelectBox,
   SettingsImportScreen,
   SettingsScreen,
-  SolutionForm,
   SvgIcon,
   Tab,
   Tabs,
