1ad8533a349e287464a2986d54cc8f2d583b24d4
Bernardo Dias <bernardodiasdacruz@gmail.com>
2019-09-05 11:49:29 -0300

    Remove unused UI components (#430)
    * Remove unused UI components
    
    * Include build

--- src/components/ui/RoadmapTimelineElement.stories.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { storiesOf } from '@storybook/react'
-
-import RoadmapTimelineElement from './RoadmapTimelineElement'
-
-storiesOf('UI Components/RoadmapTimelineElement', module)
-  .add('basic usage', () => <RoadmapTimelineElement />)
-  .add('active state', () => <RoadmapTimelineElement isDone />)
-  .add('missing props (does component explode?)', () => <RoadmapTimelineElement />)
--- src/index.js
+++ src/index.js
@@ -10,7 +10,6 @@ import ApplicantScreen from './components/ui/ApplicantScreen'
 import AttachFiles from './components/ui/AttachFiles'
 import Avatar from './components/ui/Avatar'
 import Button from './components/ui/Button'
-import CollapsibleSection from './components/ui/CollapsibleSection'
 import ColorPalette from './components/ui/ColorPalette'
 import ColumnsCustomizer from './components/ui/ColumnsCustomizer'
 import DataGrid from './components/ui/DataGrid'
@@ -19,7 +18,6 @@ import Email from './components/ui/Email'
 import EmailFeed from './components/ui/EmailFeed'
 import ErrorBox from './components/ui/ErrorBox'
 import Filters from './components/ui/Filters'
-import FilterTag from './components/ui/FilterTag'
 import Footer from './components/ui/Footer/Footer'
 import FooterBrands from './components/ui/Footer/FooterBrands'
 import FooterList from './components/ui/Footer/FooterList'
@@ -33,12 +31,10 @@ import InlineEditor from './components/ui/InlineEditor'
 import InputField from './components/forms/InputField'
 import InputGroup from './components/forms/InputGroup'
 import IssueCollectorButton from './components/ui/IssueCollectorButton'
-import Keywords from './components/ui/Keywords'
 import ListExclusionFormPopup from './components/ui/ListExclusionFormPopup'
 import ListsEditor from './components/ui/ListsEditor'
 import Loader from './components/ui/Loader'
 import MetaGroup from './components/ui/MetaGroup'
-import Milestones from './components/ui/Milestones'
 import MilestonesScreen from './components/ui/MilestonesScreen'
 import Modal from './components/ui/Modal'
 import Note from './components/ui/Note'
@@ -48,16 +44,11 @@ import ProfileHeaderLinks from './components/ui/ProfileHeaderLinks'
 import QuickSearchCloseButton from './components/ui/QuickSearchCloseButton'
 import ResultCount from './components/ui/ResultCount'
 import RichTextEditor from './components/ui/RichTextEditor'
-import Roadmap from './components/ui/Roadmap'
 import RoadmapHero from './components/ui/RoadmapHero'
-import RoadmapLevel from './components/ui/RoadmapLevel'
-import RoadmapTimelineElement from './components/ui/RoadmapTimelineElement'
-import SearchForm from './components/ui/SearchForm'
 import SearchResult from './components/ui/SearchResult'
 import SelectBox from './components/ui/SelectBox'
 import SettingsImportScreen from './components/ui/SettingsImportScreen'
 import SettingsScreen from './components/ui/SettingsScreen'
-import SolutionForm from './components/forms/SolutionForm'
 import SvgIcon from './components/ui/SvgIcon'
 import Tab from './components/ui/Tabs/Tab'
 import Tabs from './components/ui/Tabs/Tabs'
@@ -80,7 +71,6 @@ export {
   AttachFiles,
   Avatar,
   Button,
-  CollapsibleSection,
   ColorPalette,
   ColumnsCustomizer,
   DataGrid,
@@ -89,7 +79,6 @@ export {
   EmailFeed,
   ErrorBox,
   Filters,
-  FilterTag,
   Footer,
   FooterBrands,
   FooterList,
@@ -103,12 +92,10 @@ export {
   InputField,
   InputGroup,
   IssueCollectorButton,
-  Keywords,
   ListExclusionFormPopup,
   ListsEditor,
   Loader,
   MetaGroup,
-  Milestones,
   MilestonesScreen,
   Modal,
   Note,
@@ -118,16 +105,11 @@ export {
   QuickSearchCloseButton,
   ResultCount,
   RichTextEditor,
-  Roadmap,
   RoadmapHero,
-  RoadmapLevel,
-  RoadmapTimelineElement,
-  SearchForm,
   SearchResult,
   SelectBox,
   SettingsImportScreen,
   SettingsScreen,
-  SolutionForm,
   SvgIcon,
   Tab,
   Tabs,
