48fb31a1fa8a8352e97f4e8a79e0d05b155c33c8
Bernardo Dias <bernardodiasdacruz@gmail.com>
2019-09-23 08:28:07 -0300

    Adjustments for code review

--- src/components/ui/LoginButton.js
+++ src/components/ui/LoginButton.js
@@ -8,29 +8,29 @@ import Button from './Button'
 const cmz = require('cmz')
 
 type Props = {
-  id?: String,
+  id?: string,
   children?: React$Node
 }
 
 const cx = {
   button: cmz(`
-    border-radius: 3px;
-    border: 2px solid transparent;
-    cursor: pointer;
-    display: inline-block;
-    font-weight: 700;
-    line-height: 1.5;
-    outline: none;
-    min-width: 290px;
-    margin: .15em auto;
-    text-decoration: none;
-    text-transform: uppercase;
-    transition: all .3s ease-out;
-    white-space: nowrap;
-    font-family: 'Raleway', sans-serif;
-    border-width: 2px;
-    font-size: 1rem;
-    padding: .75em 2.4em;
+    border-radius: 3px
+    border: 2px solid transparent
+    cursor: pointer
+    display: inline-block
+    font-weight: 700
+    line-height: 1.5
+    outline: none
+    min-width: 290px
+    margin: .15em auto
+    text-decoration: none
+    text-transform: uppercase
+    transition: all .3s ease-out
+    white-space: nowrap
+    font-family: 'Raleway', sans-serif
+    border-width: 2px
+    font-size: 1rem
+    padding: .75em 2.4em
   `)
 }
 
--- src/components/ui/LoginButton.stories.js
+++ src/components/ui/LoginButton.stories.js
@@ -1,12 +1,18 @@
+// @flow
+
 import React from 'react'
 import { storiesOf } from '@storybook/react'
 
 import LoginButton from './LoginButton'
 
 storiesOf('UI Components/LoginButton', module)
-  .add('basic usage', () => <LoginButton children='Login With Google' id='google-callback-id' />)
-  .add('missing props (does component explode?)', () => {
-    return (
-      <LoginButton />
-    )
-  })
+  .add('basic usage', () => (
+    <LoginButton id='google-callback-id'>
+      Login With Google
+    </LoginButton>
+  ))
+
+storiesOf('UI Components/LoginButton/Debug', module)
+  .add('missing props (does component explode?)', () => (
+    <LoginButton />
+  ))
