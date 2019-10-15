aa057be9f217c79b16766ac0dfe8edff7645e5bb
Bernardo Dias <bernardodiasdacruz@gmail.com>
2019-10-15 08:49:00 -0300

    Update ApplicantScreen component

--- src/components/ui/ApplicantScreen.js
+++ src/components/ui/ApplicantScreen.js
@@ -12,22 +12,46 @@ const cx = {
   content: cmz(`
     max-width: 1280px
     margin: 93px auto 0
+  `),
+
+  narrower: cmz(`
+    max-width: 840px
+    margin: 93px auto
+    padding: 0 60px 80px 60px
+  `),
+
+  wider: cmz(`
+    max-width: 964px
+    margin: 0 auto
+    padding: 0 60px
+    padding-top: 118px
   `)
 }
 
 type Props = {
   children?: React$Node,
-  contentWrapper?: boolean
+  noWrapper?: boolean,
+  wrapper?: 'narrower' | 'wider'
 }
 
 const ApplicantScreen = (props: Props) => {
-  const { children, contentWrapper = true } = props
+  const { children, noWrapper = false, wrapper } = props
+  const getWrapperClass = () => {
+    switch (wrapper) {
+      case 'narrower':
+        return cx.narrower
+      case 'wider':
+        return cx.wider
+      default:
+        return cx.content
+    }
+  }
   return (
     <div>
       <HeaderBar />
-      {contentWrapper ? (
-        <div className={cx.content}>{children}</div>
-      ) : children}
+      {noWrapper ? children : (
+        <div className={getWrapperClass()}>{children}</div>
+      )}
     </div>
   )
 }
