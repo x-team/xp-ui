a9edd612358fdb9fdd946897bdef5dd4de5b9fd5
Nick Plekhanov <nickatgit@yandex.com>
2019-11-01 20:18:35 +0300

    Remove excessive components from the registration bundle

--- src/registration.js
+++ src/registration.js
@@ -1,25 +1,14 @@
 import ApplicantScreen from './components/ui/ApplicantScreen'
-import ApplicantScreenNotification from './components/ui/ApplicantScreenNotification'
 import AttachFiles from './components/ui/AttachFiles'
-import Avatar from './components/ui/Avatar'
 import Button from './components/ui/Button'
-import ColumnsCustomizer from './components/ui/ColumnsCustomizer'
-import Dropdown from './components/ui/Dropdown'
-import Email from './components/ui/Email'
-import EmailFeed from './components/ui/EmailFeed'
 import ErrorBox from './components/ui/ErrorBox'
-import Filters from './components/ui/Filters'
 import Footer from './components/ui/Footer/Footer'
 import FooterBrands from './components/ui/Footer/FooterBrands'
 import FooterList from './components/ui/Footer/FooterList'
-import GenericCollapsible from './components/ui/GenericCollapsible'
-import GenericCopyToClipboard from './components/ui/GenericCopyToClipboard'
-import GenericTabs from './components/ui/GenericTabs'
 import HeaderBar from './components/ui/HeaderBar'
 import HorizontalRuler from './components/ui/HorizontalRuler'
 import InputField from './components/forms/InputField'
 import InputGroup from './components/forms/InputGroup'
-import IssueCollectorButton from './components/ui/IssueCollectorButton'
 import JobApplicationCard from './components/ui/JobApplicationCard'
 import JobCard from './components/ui/JobCard'
 import JobDetails from './components/ui/JobDetails'
@@ -27,44 +16,23 @@ import JobsGrid from './components/ui/JobsGrid'
 import JobsPageBreadcrumbs from './components/ui/JobsPageBreadcrumbs'
 import JobsPageLayout from './components/ui/JobsPageLayout'
 import Loader from './components/ui/Loader'
-import MetaGroup from './components/ui/MetaGroup'
-import MilestonesScreen from './components/ui/MilestonesScreen'
 import Modal from './components/ui/Modal'
-import PencilButton from './components/ui/PencilButton'
-import ProfileHeaderLinks from './components/ui/ProfileHeaderLinks'
-import ResultCount from './components/ui/ResultCount'
 import RoadmapHero from './components/ui/RoadmapHero'
-import SvgIcon from './components/ui/SvgIcon'
-import Tab from './components/ui/Tabs/Tab'
-import Tabs from './components/ui/Tabs/Tabs'
 import Text from './components/ui/Text'
 import TruncatedList from './components/ui/TruncatedList'
-import TwoColumnsLayout from './components/ui/TwoColumnsLayout'
-import XIcon from './components/ui/XIcon'
 
 export {
   ApplicantScreen,
-  ApplicantScreenNotification,
   AttachFiles,
-  Avatar,
   Button,
-  ColumnsCustomizer,
-  Dropdown,
-  Email,
-  EmailFeed,
   ErrorBox,
-  Filters,
   Footer,
   FooterBrands,
   FooterList,
-  GenericCollapsible,
-  GenericCopyToClipboard,
-  GenericTabs,
   HeaderBar,
   HorizontalRuler,
   InputField,
   InputGroup,
-  IssueCollectorButton,
   JobApplicationCard,
   JobCard,
   JobDetails,
@@ -72,18 +40,8 @@ export {
   JobsPageBreadcrumbs,
   JobsPageLayout,
   Loader,
-  MetaGroup,
-  MilestonesScreen,
   Modal,
-  PencilButton,
-  ProfileHeaderLinks,
-  ResultCount,
   RoadmapHero,
-  SvgIcon,
-  Tab,
-  Tabs,
   Text,
-  TruncatedList,
-  TwoColumnsLayout,
-  XIcon
+  TruncatedList
 }
