9c00255592e5ac81aeaec7e2d28dd2a9fa6da277
Wagner Moreira <wmoreiradev@gmail.com>
2019-11-05 07:57:25 -0400

    Create ConfirmationBox UI component (#467)
    * create ConfirmationBox component and its stories
    
    * improve stories, fix flow check errors
    
    * update confirmationBox component, order css and remove unused StoryModal from stories file
    
    * generate build files, update npm storybook/addon-storyshots and addon-info packages because of npm audit warnings
    
    * generate build files
    
    * add a new variation of gray to the theme file, generate build files
    
    * change guard operators to check separately action and dismiss buttons
    
    * add separate stories to showacse each button rendered separately
    
    * change dismiss button css to uppercase
    
    * update story name
    
    * Revert package changes
    
    * List components in alphabetical order
    
    * Fix linting errors
    
    * Use natural typing notation
    
    * Build app
    
    * address pr comments and generate build
    
    * update storyshots
    
    * address pr comments and add css for mobile making text wrap
    
    * remove button class because it is not needed anymore
    
    * generate build files
    
    * replace css media query for a simpler way
    
    * generate build files
    
    * Update package-lock.json
    
    * Revert package-lock.json changes

---
+++ src/components/ui/ConfirmationBox.js
@@ -0,0 +1,88 @@
+// @flow
+
+import React from 'react'
+
+import Button from './Button'
+
+import theme from '../../styles/theme'
+import { typeface } from '../../styles/typo'
+
+const cmz = require('cmz')
+
+type Props = {
+  title?: string,
+  content?: string,
+  action?: () => void,
+  actionLabel?: string,
+  dismissAction?: () => void
+}
+
+const WRAPPER_PADDING = '24px'
+const cx = {
+  wrapper: cmz(`
+    padding: ${WRAPPER_PADDING}
+    width: calc(100% - ${WRAPPER_PADDING} * 2)
+    max-width: calc(500px - ${WRAPPER_PADDING} * 2)
+  `),
+
+  title: cmz(
+    typeface.semiHeading, `
+    color: ${theme.typoHeading}
+    font-size: 24px
+    font-weight: 500
+    margin: 0
+  `),
+
+  content: cmz(
+    typeface.text,
+    `
+    color: ${theme.typoParagraph}
+    font-weight: normal
+    font-size: 20px
+    line-height: 140%
+    margin: 24px 0
+  `),
+
+  dismissButton: cmz(`
+    & {
+      color: ${theme.typoLabel}
+      margin-left: 10px
+    }
+
+    &:only-child {
+      margin: 0
+    }
+  `),
+
+  dismissButtonLabel: cmz(`
+    text-transform: uppercase
+  `)
+}
+
+const ConfirmationBox = ({ title = 'Are you sure?', content, action, actionLabel, dismissAction }: Props) => (
+  <div className={cx.wrapper}>
+    <h2 className={cx.title}>{title}</h2>
+    <p className={cx.content}>{content}</p>
+    {action && (
+      <Button
+        onClick={action}
+      >
+        {actionLabel}
+      </Button>
+    )}
+
+    {dismissAction && (
+      <Button
+        pseudolink
+        className={cx.dismissButton}
+        onClick={dismissAction}
+      >
+        <span className={cx.dismissButtonLabel}>
+          Dismiss
+        </span>
+      </Button>
+    )}
+  </div>
+)
+
+export default ConfirmationBox
