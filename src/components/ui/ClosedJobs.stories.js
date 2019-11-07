0084309159c6bedb267c95f6649ea39ad0cf4866
Wagner Moreira <wmoreiradev@gmail.com>
2019-11-07 13:31:43 -0400

    Create ClosedJobs component (#470)
    * Create ClosedJobs component and its stories file
    
    * Add AppLink
    
    * Remove lightTuna color from theme and apply opacity to tuna instead
    
    * Generate build files
    
    * Update storyshots
    
    * Improve css code, make arquived applications message a prop
    
    * Change font-size to rem, font-weight to use numbers, line-height to use unitless values
    
    * Generate build files
    
    * add more stories to showcase missing props
    
    * change story name
    
    * revert package-lock file to develop state
    
    * address pr review comments and generate lib files
    
    * Update per review comment

---
+++ src/components/ui/ClosedJobs.stories.js
@@ -0,0 +1,68 @@
+// @flow
+/* global React$Node */
+
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+import { text } from '@storybook/addon-knobs'
+import { action } from '@storybook/addon-actions'
+import faker from 'faker'
+
+import ClosedJobs from './ClosedJobs'
+
+export const JobsLink = ({ children, ...props }: { children: React$Node }) => (
+  <a onClick={action('This should be react-router/gasby Link')} {...props}>
+    {children}
+  </a>
+)
+
+const jobsList = [
+  {
+    title: 'Web Developer with Ruby on Rails experience',
+    description: 'Closed on April 17th, 2019, applied on March 28th, 2019'
+  },
+  {
+    title: 'Senior Full Stack Engineer',
+    description: 'Closed on February 17th, 2019, applied on March 28th, 2019'
+  },
+  {
+    title: 'UX Content Writer',
+    description: 'Closed on April 17th, 2019, applied on March 28th, 2019'
+  }
+]
+
+const randomData = [
+  {
+    title: text('Title', faker.name.jobTitle()),
+    description: text('Description', faker.lorem.sentence())
+  },
+  {
+    title: text('Title', faker.name.jobTitle()),
+    description: text('Description', faker.lorem.sentence())
+  },
+  {
+    title: text('Title', faker.name.jobTitle()),
+    description: text('Description', faker.lorem.sentence())
+  }
+]
+
+storiesOf('UI Components|ClosedJobs', module)
+  .add('basic usage', () => (
+    <ClosedJobs jobs={jobsList} link={JobsLink} message={'Applications older than 3 months are archived'} />
+  ))
+
+storiesOf('UI Components|ClosedJobs/Debug', module)
+  .add('with random data', () => (
+    <ClosedJobs jobs={randomData} link={JobsLink} message={text('message', faker.lorem.sentence())} />
+  ))
+  .add('with jobs only', () => (
+    <ClosedJobs jobs={jobsList} />
+  ))
+  .add('with archived message only', () => (
+    <ClosedJobs message={'Applications older than 3 months are archived'} />
+  ))
+  .add('with link only', () => (
+    <ClosedJobs link={JobsLink} />
+  ))
+  .add('missing props', () => (
+    <ClosedJobs />
+  ))
