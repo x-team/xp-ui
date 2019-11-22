2334fb4f260f18c0d086a91734682b9babe513bb
Bernardo Dias <bernardodiasdacruz@gmail.com>
2019-11-22 13:05:20 -0300

    Add new WelcomeHero component (#482)
    * Add new WelcomeHero component
    
    * Update VideoPlayer
    
    * Update stories for VideoPlayer and WelcomeHero
    
    * Update VideoPlayer state name
    
    * Remove extra empty line
    
    * Update logic structure for VideoPlayer
    
    * Build app
    
    * Code updates
    
    # Conflicts:
    #	lib/all.js
    #	lib/all.js.map
    #	lib/editor.js
    #	lib/editor.js.map
    #	lib/fonts.js
    #	lib/fonts.js.map
    #	lib/registration.js
    #	lib/registration.js.map

---
+++ src/components/ui/WelcomeHero.js
@@ -0,0 +1,187 @@
+// @flow
+/* global React$Element */
+
+import React from 'react'
+
+import Text from './Text'
+import VideoPlayer from './VideoPlayer'
+import GenericTooltip from './GenericTooltip'
+
+import theme, { breakpoints } from '../../styles/theme'
+import typo from '../../styles/typo'
+
+const cmz = require('cmz')
+
+const GAP = '32px'
+const WRAPPER_WIDTH = '1100px'
+
+const cx = {
+  wrapper: cmz(
+    typo.regularText,
+    `
+      background: ${theme.baseBrightSilver}
+    `
+  ),
+
+  container: cmz(`
+    & {
+      display: flex
+      flex-direction: column
+      align-items: center
+      max-width: calc(${WRAPPER_WIDTH} - 2 * ${GAP})
+      padding: ${GAP}
+      margin: 0 auto
+    }
+
+    @media screen and (min-width: ${breakpoints.md}) {
+      & {
+        flex-direction: row
+      }
+    }
+  `),
+
+  content: cmz(`
+    width: 100%
+  `),
+
+  heading: cmz(
+    typo.heading,
+    `
+      margin: 0 0 -16px
+      font-size: 42px
+      line-height: 57px
+      letter-spacing: -0.02em
+    `
+  ),
+
+  text: cmz(`
+    font-size: 24px
+    line-height: 1.4
+    margin: -11px 0 0 
+  `),
+
+  player: cmz(`
+    & {
+      display: block
+      align-self: flex-start
+      box-shadow: -10px 10px 0 ${theme.baseTuna.fade(0.95)}
+      background: ${theme.baseTuna.fade(0.85)}
+      margin: ${GAP} auto 10px
+    }
+
+    & > iframe {
+      display: block
+    }
+
+    @media screen and (min-width: ${breakpoints.md}) {
+      & {
+        margin: 0 0 10px ${GAP}
+      }
+    }
+  `),
+
+  actions: cmz(`
+    & {
+      display: flex
+      align-items: center
+      margin: 24px 0 0
+    }
+
+    & > :first-child:not(:only-child) {
+      margin: 0 48px 0 0
+    }
+  `),
+
+  profile: cmz(`
+    & {
+      text-decoration: none
+      color: ${theme.typoAnchor}
+      font-size: 20px
+      font-weight: 600
+      white-space: nowrap
+      flex-wrap: wrap
+      line-height: 1.4
+    }
+
+    &:hover {
+      color: ${theme.typoAnchorHover}
+    }
+  `),
+
+  dismiss: cmz(`
+    & {
+      color: ${theme.typoParagraph.fade(0.5)}
+      background: none
+      font-size: 20px
+      text-transform: none
+      border: none
+      font-weight: 500
+      cursor: pointer
+    }
+
+    &:hover {
+      background: none
+    }
+  `)
+}
+
+type Props = {
+  heading?: string,
+  videoUrl?: string,
+  onDismiss?: () => void,
+  children?: React$Element<*>|string
+}
+
+const WelcomeHero = ({ heading, videoUrl, onDismiss, children }: Props) => heading || children ? (
+  <div className={cx.wrapper}>
+    <div className={cx.container}>
+      <div className={cx.content}>
+        <Text
+          heading={heading}
+          headingClass={cx.heading}
+          content={children}
+          contentClass={cx.text}
+          hasDivider={!!heading}
+        />
+        <div className={cx.actions}>
+          <GenericTooltip
+            message={
+              <span>
+                We currently heavily rely on LinkedIn
+                <br />
+                Profiles to match candidates with
+                <br />
+                potential roles.
+              </span>
+            }
+          >
+            <a
+              className={cx.profile}
+              href='https://www.linkedin.com/in/'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              Update your LinkedIn profile »
+            </a>
+          </GenericTooltip>
+          {onDismiss && (
+            <div className={cx.dismiss} onClick={onDismiss}>Dismiss</div>
+          )}
+        </div>
+      </div>
+      {videoUrl && (
+        <div className={cx.player}>
+          <VideoPlayer
+            src={videoUrl}
+            embedded
+            overlay
+            width={534}
+            height={300}
+          />
+        </div>
+      )}
+    </div>
+  </div>
+) : null
+
+export default WelcomeHero
--- src/registration.js
+++ src/registration.js
@@ -32,6 +32,7 @@ import ResultCount from './components/ui/ResultCount'
 import RoadmapHero from './components/ui/RoadmapHero'
 import Text from './components/ui/Text'
 import TruncatedList from './components/ui/TruncatedList'
+import WelcomeHero from './components/ui/WelcomeHero'
 
 export {
   ApplicantScreen,
@@ -67,5 +68,6 @@ export {
   ResultCount,
   RoadmapHero,
   Text,
-  TruncatedList
+  TruncatedList,
+  WelcomeHero
 }
