e7808f61d31e0f3005e36f9cf247579efb86054d
Nick Plekhanov <nickatgit@yandex.com>
2019-12-17 19:23:15 +0300

    Remove excessive components from the registration bundle

--- src/registration.js
+++ src/registration.js
@@ -1,5 +1,4 @@
 import ApplicantScreen from './components/ui/ApplicantScreen'
-import ApplicantScreenNotification from './components/ui/ApplicantScreenNotification'
 import AttachFiles from './components/ui/AttachFiles'
 import Button from './components/ui/Button'
 import ClosedJobApplications from './components/ui/ClosedJobApplications'
@@ -8,12 +7,9 @@ import ErrorBox from './components/ui/ErrorBox'
 import Footer from './components/ui/Footer/Footer'
 import FooterBrands from './components/ui/Footer/FooterBrands'
 import FooterList from './components/ui/Footer/FooterList'
-import GenericTooltip from './components/ui/GenericTooltip'
-import HeaderBar from './components/ui/HeaderBar'
 import HorizontalRuler from './components/ui/HorizontalRuler'
 import InputField from './components/forms/InputField'
 import InputGroup from './components/forms/InputGroup'
-import IssueCollectorButton from './components/ui/IssueCollectorButton'
 import JobApplicationCard from './components/ui/JobApplicationCard'
 import JobCard from './components/ui/JobCard'
 import JobDetails from './components/ui/JobDetails'
@@ -23,20 +19,13 @@ import JobsPageLayout from './components/ui/JobsPageLayout'
 import JobSkills from './components/ui/JobSkills'
 import ProTipCard from './components/ui/ProTipCard'
 import Loader from './components/ui/Loader'
-import MetaGroup from './components/ui/MetaGroup'
-import MilestonesScreen from './components/ui/MilestonesScreen'
 import Modal from './components/ui/Modal'
-import PencilButton from './components/ui/PencilButton'
-import ProfileHeaderLinks from './components/ui/ProfileHeaderLinks'
-import ResultCount from './components/ui/ResultCount'
-import RoadmapHero from './components/ui/RoadmapHero'
 import Text from './components/ui/Text'
 import TruncatedList from './components/ui/TruncatedList'
 import WelcomeHero from './components/ui/WelcomeHero'
 
 export {
   ApplicantScreen,
-  ApplicantScreenNotification,
   AttachFiles,
   Button,
   ClosedJobApplications,
@@ -45,12 +34,9 @@ export {
   Footer,
   FooterBrands,
   FooterList,
-  GenericTooltip,
-  HeaderBar,
   HorizontalRuler,
   InputField,
   InputGroup,
-  IssueCollectorButton,
   JobApplicationCard,
   JobCard,
   JobDetails,
@@ -60,13 +46,7 @@ export {
   JobSkills,
   ProTipCard,
   Loader,
-  MetaGroup,
-  MilestonesScreen,
   Modal,
-  PencilButton,
-  ProfileHeaderLinks,
-  ResultCount,
-  RoadmapHero,
   Text,
   TruncatedList,
   WelcomeHero
