cfe9baafdfdb919b5fb8ca27c1e057f53b660a77
Dener Miranda <denner.miranda@gmail.com>
2020-04-20 15:14:46 -0300

    Add component

---
+++ src/components/ui/Label.js
@@ -0,0 +1,37 @@
+// @flow
+import React from 'react'
+import theme from '../../styles/theme'
+import typo from '../../styles/typo'
+const cmz = require('cmz')
+const cx = {
+  headline: cmz(
+    typo.baseText,
+    `
+      font-size: 24px
+    `
+  ),
+  error: cmz(`
+    color: ${theme.typoSubheading}
+  `),
+  description: cmz(
+    typo.baseText,
+    `
+      font-size: 18px
+    `
+  )
+}
+type Props = {
+    headline?: String,
+    description?: String,
+    hasError?: Boolean
+}
+const Label = ({ headline, description, hasError }: Props) => {
+  const headlineClassname = hasError ? [cx.headline, cx.error].join(' ') : cx.headline
+  return (
+    <div>
+      <div className={headlineClassname}>{headline}</div>
+      <div className={cx.description}>{description}</div>
+    </div>
+  )
+}
+export default Label
---
+++ src/components/ui/Label.stories.js
@@ -0,0 +1,37 @@
+// @flow
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+import Label from './Label'
+storiesOf('Core Components|Form Components/Label', module)
+  .add('basic usage', () => (
+    <Label
+      headline='Lorem ipsum'
+      description='Lorem ipsum lorem ipsum lorem ipsum lorem ipsum'
+    />
+  ))
+storiesOf('Core Components|Form Components/Label/Debug', module)
+  .add('with error', () => (
+    <Label
+      headline='Lorem ipsum'
+      description='Lorem ipsum lorem ipsum lorem ipsum lorem ipsum'
+      hasError
+    />
+  ))
+  .add('without descriptions', () => (
+    <Label
+      headline='Lorem ipsum'
+    />
+  ))
+  .add('without headline', () => (
+    <Label
+      description='Lorem ipsum lorem ipsum lorem ipsum lorem ipsum'
+    />
+  ))
+  .add('without very long headline', () => (
+    <Label
+      headline='Lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum '
+    />
+  ))
+  .add('missing props', () => (
+    <Label />
+  ))
