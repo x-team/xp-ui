fe074e5638835b48f360a72ec1ddd20b21caf8c3
Marlo Vinall Richardson <marlo@janedoe.co>
2020-04-28 11:13:34 +1000

    Add data-testid to empty state wrapper
    Why:
    • Required to test a 'no applications' state

--- src/components/ui/EmptyState.js
+++ src/components/ui/EmptyState.js
@@ -87,7 +87,7 @@ type Props = {
 }
 
 const EmptyState = ({ children, heading }: Props) => (
-  <div className={cx.wrapper}>
+  <div data-testid='xpui-emptyState-wrapper' className={cx.wrapper}>
     <svg className={cx.image} width='124' height='103' viewBox='0 0 124 103' fill='none'>
       <path d='M62 103C75.2297 103 85.9545 99.5492 85.9545 95.2925C85.9545 91.0358 75.2297 87.585 62 87.585C48.7702 87.585 38.0454 91.0358 38.0454 95.2925C38.0454 99.5492 48.7702 103 62 103Z' fill='#F0F1F4' />
       <path fillRule='evenodd' clipRule='evenodd' d='M0 20.214L20.3671 32.1052L124 0L0 20.214Z' fill='#F0F1F4' />
