f0b3f2e7fa20cdbff860981420c149000beae913
Carlos Almeida <carlosalmeida.xon@gmail.com>
2020-05-06 15:02:38 -0400

    refactored due to code-review

--- src/components/ui/WorkExperienceList.js
+++ src/components/ui/WorkExperienceList.js
@@ -2,70 +2,146 @@
 
 import React, { PureComponent } from 'react'
 
-import WorkExperienceCard from './WorkExperienceCard'
+import theme from '../../styles/theme'
+import typo, { typeface } from '../../styles/typo'
 
 const cmz = require('cmz')
 
 const cx = {
-  item: cmz(
+  wrapper: cmz(`
+    & {
+      background: ${theme.white}
+      border: 1px solid ${theme.lineSilver5}
+      box-shadow: 4px 4px 0px ${theme.baseTuna.fade(0.95)}
+      padding: 24px
+      margin: 0 0 24px 0
+    }
+
+    &:last-child {
+      margin: 0
+    }
+  `),
+
+  label: cmz(
+    typo.baseText,
+    `
+      margin-top: -6px
+    `
+  ),
+
+  role: cmz(`
+    font-weight: 500
+  `),
+
+  at: cmz(`
+    font-style: italic
+    margin: 0px 6px
+  `),
+
+  company: cmz(`
+    font-weight: 400
+  `),
+
+  dates: cmz(
+    typo.baseText,
+    `
+      font-size: 18px
+      margin-top: 5px
+    `
+  ),
+
+  buttons: cmz(
+    typeface.extra,
     `
       & {
-        margin: 0 0 24px 0
+        margin: 12px 0 -2px 0
+        display: flex
       }
 
-      &:last-child {
-        margin: 0
+      & div {
+        text-transform: uppercase
+        line-height: 18px
+        font-size: 13px
+        cursor: pointer
+      }
+
+      & div:hover {
+        text-decoration: underline
       }
     `
-  )
+  ),
+
+  buttonEdit: cmz(`
+    color: ${theme.baseRed}
+  `),
+
+  buttonDelete: cmz(`
+    color: ${theme.typoHeaderAnchor}
+    margin: 0 0 0 15px
+  `)
+}
+
+const monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+
+type WorkExperienceCard = {
+  id: number,
+  role?: string,
+  company?: string,
+  startDate?: Date,
+  endDate?: Date
 }
 
 type Props = {
-  list: Array<any>,
-  returnEditingCard?: (any) => void
+  list: Array<WorkExperienceCard>,
+  editEntry?: (number) => void,
+  deleteEntry?: (number) => void
 }
 
-type State = {
-  editingCard: any
-}
-
-class WorkExperienceList extends PureComponent<Props, State> {
+class WorkExperienceList extends PureComponent<Props> {
   static defaultProps = {
     list: []
   }
 
-  state: State = {
-    editingCard: {}
+  formatDate = (date: Date): string => {
+    return date && `${monthNames[date.getMonth()]} ${date.getFullYear()}`
   }
 
-  componentDidUpdate (prevProps: any, prevState: any) {
-    if (this.props.returnEditingCard) {
-      this.props.returnEditingCard(this.state)
-    }
+  handleEditEntry = (id: number) => (): void => {
+    const { editEntry } = this.props
+    editEntry && editEntry(id)
   }
 
-  getEditingCardId = (editingCardId: number) => {
-    this.setState({
-      editingCard: this.props.list.filter((workExperienceCard) => workExperienceCard.id === editingCardId)
-    })
+  handleDeleteEntry = (id: number) => (): void => {
+    const { deleteEntry } = this.props
+    deleteEntry && deleteEntry(id)
   }
 
   render () {
-    const { list, returnEditingCard } = this.props
+    const { list, editEntry, deleteEntry } = this.props
 
     return (
-      (list && returnEditingCard)
-        ? <div>
-          {
-            list.map((workExperienceCard) => {
-              return (
-                <div className={cx.item} key={workExperienceCard.id}>
-                  <WorkExperienceCard {...workExperienceCard} returnEditingCardId={this.getEditingCardId} />
+      list
+        ? list.map((workExperienceCard) => {
+          const { id, role, company, startDate, endDate } = workExperienceCard
+
+          return (
+            <div className={cx.wrapper} key={id}>
+              <div className={cx.label}>
+                {role && <span className={cx.role}>{role}</span>}
+                {(role && company) && <span className={cx.at}>at</span>}
+                {company && <span className={cx.company}>{company}</span>}
+              </div>
+              {startDate && <div className={cx.dates}>{this.formatDate(startDate)} - {endDate ? this.formatDate(endDate) : 'Current'}</div>}
+              {
+                (editEntry && deleteEntry) &&
+                <div className={cx.buttons}>
+                  <div className={cx.buttonEdit} onClick={this.handleEditEntry(id)}>Edit Entry</div>
+                  <div className={cx.buttonDelete} onClick={this.handleDeleteEntry(id)}>Delete</div>
                 </div>
-              )
-            })
-          }
-        </div>
+              }
+            </div>
+          )
+        })
         : null
     )
   }
--- src/components/ui/WorkExperienceList.stories.js
+++ src/components/ui/WorkExperienceList.stories.js
@@ -1,13 +1,18 @@
-// @flow alert
-/* globals alert */
+// @flow
 
 import React from 'react'
 import { storiesOf } from '@storybook/react'
 
 import WorkExperienceList from './WorkExperienceList'
 
-const getEditingCardParentFunction = (cardData) => {
-  alert(`Editing Card: ${JSON.stringify(cardData)}`)
+const handleEditExperience = cardId => {
+  // do stuff experience item with this `cardid`
+  console.log(`Edit Card ID: ${cardId}`)
+}
+
+const handleDeleteExperience = cardId => {
+  // do stuff experience item with this `cardid`
+  console.log(`Delete Card ID: ${cardId}`)
 }
 
 const experiencesFromApiMocked = [
@@ -47,25 +52,187 @@ const experiencesFromApiMocked = [
   }
 ]
 
-storiesOf('Core Components|WorkExperienceList Component', module)
+storiesOf('UI Components|WorkExperienceList', module)
   .add('basic usage', () => (
     <WorkExperienceList
       list={experiencesFromApiMocked}
-      returnEditingCard={getEditingCardParentFunction}
+      editEntry={handleEditExperience}
+      deleteEntry={handleDeleteExperience}
     />
   ))
+  .add('basic usage with just one 1 experience', () => {
+    const list = [{
+      id: 5,
+      role: 'Senior Cheff',
+      company: 'Pizza Hut',
+      startDate: new Date('2013-09-06'),
+      endDate: new Date('2014-02-15')
+    }]
 
-storiesOf('Core Components|WorkExperienceList Component/Debug', module)
-  .add('without list', () => (
+    return (
+      <WorkExperienceList
+        list={list}
+        editEntry={handleEditExperience}
+        deleteEntry={handleDeleteExperience}
+      />
+    )
+  })
+
+storiesOf('UI Components|WorkExperienceList/Debug', module)
+  .add('missing props', () => (
+    <WorkExperienceList />
+  ))
+  .add('without list prop', () => (
     <WorkExperienceList
-      returnEditingCard={getEditingCardParentFunction}
+      editEntry={handleEditExperience}
+      deleteEntry={handleDeleteExperience}
     />
   ))
-  .add('without onEditCard function', () => (
+  .add('without editEntry prop', () => (
+    <WorkExperienceList
+      list={experiencesFromApiMocked}
+      deleteEntry={handleDeleteExperience}
+    />
+  ))
+  .add('without deleteEntry prop', () => (
+    <WorkExperienceList
+      list={experiencesFromApiMocked}
+      editEntry={handleEditExperience}
+    />
+  ))
+  .add('without editEntry and deleteEntry props', () => (
     <WorkExperienceList
       list={experiencesFromApiMocked}
     />
   ))
-  .add('missing props', () => (
-    <WorkExperienceList />
-  ))
+  .add('experience item without role', () => {
+    const list = [{
+      id: 5,
+      company: 'Pizza Hut',
+      startDate: new Date('2013-09-06'),
+      endDate: new Date('2014-02-15')
+    }]
+
+    return (
+      <WorkExperienceList
+        list={list}
+        editEntry={handleEditExperience}
+        deleteEntry={handleDeleteExperience}
+      />
+    )
+  })
+  .add('experience item without company', () => {
+    const list = [{
+      id: 5,
+      role: 'Senior Cheff',
+      startDate: new Date('2013-09-06'),
+      endDate: new Date('2014-02-15')
+    }]
+
+    return (
+      <WorkExperienceList
+        list={list}
+        editEntry={handleEditExperience}
+        deleteEntry={handleDeleteExperience}
+      />
+    )
+  })
+  .add('experience item without startDate', () => {
+    const list = [{
+      id: 5,
+      role: 'Senior Cheff',
+      company: 'Pizza Hut'
+    }]
+
+    return (
+      <WorkExperienceList
+        list={list}
+        editEntry={handleEditExperience}
+        deleteEntry={handleDeleteExperience}
+      />
+    )
+  })
+  .add('experience item without endDate', () => {
+    const list = [{
+      id: 5,
+      role: 'Senior Cheff',
+      company: 'Pizza Hut',
+      startDate: new Date('2013-09-06')
+    }]
+
+    return (
+      <WorkExperienceList
+        list={list}
+        editEntry={handleEditExperience}
+        deleteEntry={handleDeleteExperience}
+      />
+    )
+  })
+  .add('experience item with endDate', () => {
+    const list = [{
+      id: 5,
+      role: 'Senior Cheff',
+      company: 'Pizza Hut',
+      startDate: new Date('2013-09-06'),
+      endDate: new Date('2014-02-15')
+    }]
+
+    return (
+      <WorkExperienceList
+        list={list}
+        editEntry={handleEditExperience}
+        deleteEntry={handleDeleteExperience}
+      />
+    )
+  })
+  .add('experience item with long role name', () => {
+    const list = [{
+      id: 5,
+      role: 'Senior Cheff Senior Cheff Senior Cheff Senior Cheff Senior Cheff Senior Cheff Senior Cheff Senior Cheff Senior Cheff Senior Cheff Senior Cheff Senior Cheff Senior Cheff Senior Cheff Senior Cheff Senior Cheff Senior Cheff Senior Cheff Senior Cheff Senior Cheff Senior Cheff Senior Cheff Senior Cheff Senior Cheff Senior Cheff Senior Cheff',
+      company: 'Pizza Hut',
+      startDate: new Date('2013-09-06'),
+      endDate: new Date('2014-02-15')
+    }]
+
+    return (
+      <WorkExperienceList
+        list={list}
+        editEntry={handleEditExperience}
+        deleteEntry={handleDeleteExperience}
+      />
+    )
+  })
+  .add('experience item with long company name', () => {
+    const list = [{
+      id: 5,
+      role: 'Senior Cheff',
+      company: 'Pizza Hut Pizza Hut Pizza Hut Pizza Hut Pizza Hut Pizza Hut Pizza Hut Pizza Hut Pizza Hut Pizza Hut Pizza Hut Pizza Hut Pizza Hut Pizza Hut Pizza Hut Pizza Hut Pizza Hut Pizza Hut Pizza Hut Pizza Hut Pizza Hut Pizza Hut Pizza Hut Pizza Hut Pizza Hut Pizza Hut Pizza Hut Pizza Hut Pizza Hut Pizza Hut Pizza Hut Pizza Hut Pizza Hut Pizza Hut Pizza Hut Pizza Hut Pizza Hut Pizza Hut Pizza Hut Pizza Hut',
+      startDate: new Date('2013-09-06'),
+      endDate: new Date('2014-02-15')
+    }]
+
+    return (
+      <WorkExperienceList
+        list={list}
+        editEntry={handleEditExperience}
+        deleteEntry={handleDeleteExperience}
+      />
+    )
+  })
+  .add('experience item with long role and company name', () => {
+    const list = [{
+      id: 5,
+      role: 'Senior Cheff Senior Cheff Senior Cheff Senior Cheff Senior Cheff Senior Cheff Senior Cheff Senior Cheff Senior Cheff Senior Cheff Senior Cheff Senior Cheff Senior Cheff Senior Cheff Senior Cheff Senior Cheff Senior Cheff Senior Cheff Senior Cheff Senior Cheff Senior Cheff Senior Cheff Senior Cheff Senior Cheff Senior Cheff Senior Cheff',
+      company: 'Pizza Hut Pizza Hut Pizza Hut Pizza Hut Pizza Hut Pizza Hut Pizza Hut Pizza Hut Pizza Hut Pizza Hut Pizza Hut Pizza Hut Pizza Hut Pizza Hut Pizza Hut Pizza Hut Pizza Hut Pizza Hut Pizza Hut Pizza Hut Pizza Hut Pizza Hut Pizza Hut Pizza Hut Pizza Hut Pizza Hut Pizza Hut Pizza Hut Pizza Hut Pizza Hut Pizza Hut Pizza Hut Pizza Hut Pizza Hut Pizza Hut Pizza Hut Pizza Hut Pizza Hut Pizza Hut Pizza Hut',
+      startDate: new Date('2013-09-06'),
+      endDate: new Date('2014-02-15')
+    }]
+
+    return (
+      <WorkExperienceList
+        list={list}
+        editEntry={handleEditExperience}
+        deleteEntry={handleDeleteExperience}
+      />
+    )
+  })
