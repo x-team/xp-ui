c0256516cae96197318985707fcf5f357c1569bd
Jesse Bangs <jesse.bangs@x-team.com>
2020-05-07 19:18:50 +0300

    Create work experience presenter component (#531)
    * added in a basic work entry files
    
    * move the experience type definitions into the type utils
    
    * added stories for the presenter component and modified type entries
    
    * protect against the case where experience is null
    
    * export the new component from xp-ui
    
    * replaced Markdown with RichTextEditor and added task names to TODOs
    
    * updates for syntax consistency
    
    * added one trailing code convention fix

---
+++ src/components/ui/WorkExperience/WorkExperienceEntryPresenter.js
@@ -0,0 +1,106 @@
+// @flow
+
+import React from 'react'
+import cmz from 'cmz'
+
+import JobSkills from '../JobSkills'
+import RichTextEditor from '../RichTextEditor'
+
+import theme from '../../../styles/theme'
+import typo from '../../../styles/typo'
+
+import type { Experience } from '../../../utils/types'
+
+type Props = { experience: Experience }
+
+const cx = {
+  header: cmz(
+    typo.subheading,
+    `
+      & {
+        font-weight: normal
+        color: ${theme.baseDark}
+      }
+    `
+  ),
+
+  at: cmz(
+    typo.regularText,
+    `
+      & {
+        font-weight: normal
+        color: ${theme.baseGray}
+      }
+    `
+  ),
+
+  date: cmz(
+    typo.regularText,
+    `
+      & {
+        color: ${theme.baseGray}
+        margin-top: 0.4em
+        margin-bottom: 0.6em
+      }
+    `
+  ),
+
+  highlights: cmz(
+    typo.regularText,
+    `
+      & .tui-editor-contents {
+        font-size: 16px
+        color: ${theme.typoParagraph}
+        line-height: normal
+      }
+    `
+  )
+}
+
+function formatDateMonthYear (date) {
+  return date.toLocaleString('en-us', { year: 'numeric', month: 'long' })
+}
+
+function renderDates (startDate, endDate) {
+  if (startDate && endDate) {
+    return `${formatDateMonthYear(startDate)} - ${formatDateMonthYear(endDate)}`
+  }
+  if (startDate) {
+    return `Since ${formatDateMonthYear(startDate)}`
+  }
+  if (endDate) { // shouldn't happen, but just in case
+    return `Until ${formatDateMonthYear(endDate)}`
+  }
+}
+
+function WorkExperienceEntryPresenter (props: Props) {
+  if (!props.experience) {
+    return null
+  }
+
+  const {
+    experience: {
+      role,
+      company,
+      start_date: startDate,
+      end_date: endDate,
+      highlights,
+      skills
+    }
+  } = props
+
+  return (
+    <div>
+      <div className={cx.header}>
+        <strong>{role || '[No role given]'}</strong> <em className={cx.at}>at</em> <strong>{company || '[No company given]'}</strong>
+      </div>
+      <div className={cx.date}>
+        {renderDates(startDate, endDate)}
+      </div>
+      <JobSkills skills={(skills || []).join(',')} />
+      <RichTextEditor mode='viewer' initialValue={highlights} className={cx.highlights} />
+    </div>
+  )
+}
+
+export default WorkExperienceEntryPresenter
