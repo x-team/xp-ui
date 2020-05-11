e6f1fb89f96f05763e18a2f0f7254445f003318d
Jesse Bangs <jesse.bangs@x-team.com>
2020-05-11 13:50:13 +0300

    Create work experience editor (#533)
    * added in a basic work entry files
    
    * move the experience type definitions into the type utils
    
    * added stories for the presenter component and modified type entries
    
    * protect against the case where experience is null
    
    * export the new component from xp-ui
    
    * rebuild lib files
    
    * rework the Experience object to use startDate and endDate rather than start_date and end_date
    
    * implement initial version of WorkExperienceEntryEditor
    
    * additional edits for options etc
    
    * Replace TextareaEditor with RichTextEditor
    
    * updates to index and stories to handle edge cases
    
    * added a prop to cleanly start the InlineEditor in edit mode
    
    * tweaks to the editor presentation
    
    * updates to stories, correctly using the SkillsEditor and eliminating errors
    
    * make exports follow conventions
    
    * Create work experience presenter component (#531)
    
    * added in a basic work entry files
    
    * move the experience type definitions into the type utils
    
    * added stories for the presenter component and modified type entries
    
    * protect against the case where experience is null
    
    * export the new component from xp-ui
    
    * replaced Markdown with RichTextEditor and added task names to TODOs
    
    * updates for syntax consistency
    
    * added one trailing code convention fix
    
    * Minor stylistic tweaks
    
    * rebuilt libraries
    
    Co-authored-by: Carlos Almeida <carlosalmeida.xon@gmail.com>

--- src/components/ui/WorkExperience/index.js
+++ src/components/ui/WorkExperience/index.js
@@ -1,45 +1,135 @@
 // @flow
 
-import React from 'react'
+import React, { Component } from 'react'
+import cmz from 'cmz'
 
+import Button from '../Button'
 import InlineEditor from '../InlineEditor'
-
-import WorkExperienceEntryPresenter from './WorkExperienceEntryPresenter'
+import PencilButton from '../PencilButton'
 
 import type { EditorProps, PresenterProps } from '../InlineEditor'
-import type { Experience } from '../../../utils/types'
+import type { Experience, SkillOption } from '../../../utils/types'
+
+import WorkExperienceEntryPresenter from './WorkExperienceEntryPresenter'
+import WorkExperienceEntryEditor from './WorkExperienceEntryEditor'
+
+import theme from '../../../styles/theme'
+import typo from '../../../styles/typo'
 
 export type Props = {
   experiences: ?Array<Experience>,
+  skills: Array<SkillOption>,
+  onSave: ({ experience: Experience, isNew: boolean }) => Promise<any>
 }
 
-function WorkExperience (props: Props) {
-  const renderEditor = ({ value: { experience }, onValueChange, activateEditingMode, isHover }: EditorProps): any => (
-    <p>TODO: XP-3736 Create new WorkExperienceEntryEditor component</p>
-  )
+type State = {
+  showNewExperience: boolean
+}
 
-  const renderPresenter = ({ value: { experience }, activateEditingMode, isHover }: PresenterProps) => (
+// This stub experience is used as a property when creating new experience
+// entries.
+const experienceStub = {
+  role: '',
+  company: '',
+  startDate: new Date(),
+  endDate: null,
+  skills: [],
+  highlights: ''
+}
+
+const cx = {
+  root: cmz(typo.regularText),
+
+  experienceWrapper: cmz(`
+    margin-bottom: 0.8em
+    padding-top: 0.4em
+    padding-bottom: 0.6em
+    border-bottom: 1px solid ${theme.baseSilver}
+  `),
+
+  presenterWrapper: cmz(`
+    display: flex
+    flex-direction: row
+  `),
+
+  editGutter: cmz(`
+    padding-top: 0.2em
+    width: 2em
+    overflow: hide
+  `)
+}
+
+const renderEditor = ({ value: { experience, skills }, onValueChange }: EditorProps) => (
+  <WorkExperienceEntryEditor experience={experience} skills={skills}
+    onValueChanged={updatedExperience => onValueChange({ experience: updatedExperience, skills })}
+  />
+)
+
+const renderPresenter = ({ value: { experience }, activateEditingMode, isHover }: PresenterProps) => (
+  <div className={cx.presenterWrapper}>
+    <div className={cx.editGutter}>
+      {isHover && <PencilButton color='monochrome' onClick={activateEditingMode} />}
+    </div>
     <WorkExperienceEntryPresenter experience={experience} />
-  )
+  </div>
+)
 
-  const handleSave = (experience?: Experience) => {
-    // TODO: XP-3737 Add Work Experience to the XP Admin Interface
+class WorkExperience extends Component<Props, State> {
+  state = {
+    showNewExperience: false
   }
 
-  const { experiences } = props
-  return (
-    <div>
+  handleUpdate = ({ experience }: { experience: Experience }) => this.props.onSave({ experience, isNew: false })
+
+  handleSaveNew = async ({ experience }: { experience: Experience }) => {
+    try {
+      await this.props.onSave({ experience, isNew: true })
+    } finally {
+      this.setState({ showNewExperience: false })
+    }
+  }
+
+  showNewExperience = () => this.setState({ showNewExperience: true })
+
+  render () {
+    const { experiences, skills } = this.props
+    return <div className={cx.root}>
+
       {experiences && experiences.length
-        ? experiences.map(experience =>
-          <InlineEditor
-            value={{ experience }}
-            onSave={handleSave}
-            presenter={renderPresenter}
-            editor={renderEditor}
-          />)
-        : <em>No work experience provided</em>}
+        ? experiences.map((experience, index) =>
+          <div className={cx.experienceWrapper} key={index}>
+            <InlineEditor
+              value={{ experience, skills }}
+              shouldSaveOnEnter={false}
+              onSave={this.handleUpdate}
+              presenter={renderPresenter}
+              editor={renderEditor}
+            />
+          </div>
+        )
+        : <em style={{ display: 'block' }}>No work experience provided</em>
+      }
+
+      {this.state.showNewExperience
+        ? (
+          <div className={cx.experienceWrapper}>
+            <InlineEditor
+              value={{ experience: experienceStub, skills }}
+              initialMode='edit'
+              shouldSaveOnEnter={false}
+              onSave={this.handleSaveNew}
+              onCancel={() => this.setState({ showNewExperience: false })}
+              presenter={renderPresenter}
+              editor={renderEditor}
+            />
+          </div>
+        )
+        : null
+      }
+
+      <Button color='silver' onClick={this.showNewExperience}>Add New Experience</Button>
     </div>
-  )
+  }
 }
 
 export default WorkExperience
