c01c4874220986c0ac65c61bc6a59f8b201fdb2e
Bernardo Dias <bernardodiasdacruz@gmail.com>
2020-05-28 07:36:25 -0300

    Create InputError and InputSection (#546)
    * Create InputError and InputSet
    
    * Include lib files
    
    * Rename InputSet to InputSection
    
    * Add more showcases for InputSection

---
+++ src/components/forms/InputSection.stories.js
@@ -0,0 +1,72 @@
+// @flow
+
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+
+import InputSection from './InputSection'
+import InputField from './InputField'
+import Timeframe from '../ui/Timeframe'
+import SkillsSelector from '../ui/SkillsSelector'
+import RichTextEditor from '../ui/RichTextEditor'
+
+export const SampleInputSection = (props: any) => (
+  <InputSection
+    headline='Lorem ipsum dolor sit amet'
+    description='Sed dui metus, pretium vel justo at, malesuada viverra sapien.'
+    {...props}
+  >
+    <InputField placeholder='Input field' />
+  </InputSection>
+)
+
+storiesOf('Core Components|Form Components/InputSection', module)
+  .add('basic usage', () => (
+    <InputSection
+      headline='Lorem ipsum dolor sit amet'
+      description='Sed dui metus, pretium vel justo at, malesuada viverra sapien.'
+    >
+      <InputField placeholder='Input field' />
+    </InputSection>
+  ))
+
+storiesOf('Core Components|Form Components/InputSection/Use Cases', module)
+  .add('with Timeframe', () => (
+    <InputSection
+      headline='Lorem ipsum dolor sit amet'
+      description='Sed dui metus, pretium vel justo at, malesuada viverra sapien.'
+    >
+      <Timeframe />
+    </InputSection>
+  ))
+  .add('with SkillsSelector', () => (
+    <InputSection
+      headline='Lorem ipsum dolor sit amet'
+      description='Sed dui metus, pretium vel justo at, malesuada viverra sapien.'
+    >
+      <SkillsSelector />
+    </InputSection>
+  ))
+  .add('with RichTextEditor', () => (
+    <InputSection
+      headline='Lorem ipsum dolor sit amet'
+      description='Sed dui metus, pretium vel justo at, malesuada viverra sapien.'
+    >
+      <RichTextEditor />
+    </InputSection>
+  ))
+
+storiesOf('Core Components|Form Components/InputSection/States', module)
+  .add('invalid field with error message', () => (
+    <InputSection
+      headline='Lorem ipsum dolor sit amet'
+      description='Sed dui metus, pretium vel justo at, malesuada viverra sapien.'
+      errorMessage='Something went wrong.'
+    >
+      <InputField placeholder='Input field' isInvalid />
+    </InputSection>
+  ))
+
+storiesOf('Core Components|Form Components/InputSection/Debug', module)
+  .add('missing props', () => (
+    <InputSection />
+  ))
